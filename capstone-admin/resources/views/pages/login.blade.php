63a7462e2952c909a0860a9148ca80d9a3b125f2
ronrix <ronrixl5@gmail.com>
2023-02-01 16:23:10 +0800

    Feat: init
    adding form wth csrf protection

--- capstone-admin/resources/views/pages/login.blade.php
+++ capstone-admin/resources/views/pages/login.blade.php
@@ -1,7 +1,12 @@
-<x-layout class="h-screen w-full flex justify-center items-center">
-    <x-card class="w-96 mx-auto">
-        <input type="text" placeholder="username or email" class="p-3">
-        <input type="password" placeholder="password" class="p-3 my-2">
-        <input type="submit" value="login" class="bg-blue-300 p-3">
-    </x-card>
+<x-layout>
+    <div class="h-screen w-full flex justify-center items-center">
+        <x-form action="/login" class="w-96 mx-auto bg-white shadow-lg p-5" method="POST">
+            @csrf()
+            <h1 class="font-bold text-2xl uppercase">Login</h1>
+            <input type="text" name="email" placeholder="username or email" class="p-3 my-5 border border-primary-dark outline-none focus:border-red-theme">
+            <input type="password" name="password" placeholder="password" class="p-3 my-5 border border-primary-dark outline-none focus:border-red-theme">
+            <input type="submit" value="login" class="bg-primary-dark p-3 text-white font-bold text-lg">
+            <p class="text-xs text-secondary-gray text-center mt-5">login as administrator</p>
+        </x-form>
+    </div>
 </x-layout>
