b61929f0ee84894ae8589df67d1f9453ffedee08
ronrix <ronrixl5@gmail.com>
2023-02-01 16:22:20 +0800

    Feat: init
    adding fontawesome icon and dark and light theme partial element

---
+++ capstone-admin/resources/views/partials/_dnl.blade.php
@@ -0,0 +1,4 @@
+<div class="w-full flex justify-end items-center">
+    <i class="fa-solid fa-circle-half-stroke text-2xl text-primary-dark"></i>
+    <i class="fa-solid fa-circle text-2xl text-primary-dark"></i>
+</div>
