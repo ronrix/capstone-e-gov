839f774bd9c6d46eb9526944d515e4a0f446a807
ronrix <ronrixl5@gmail.com>
2023-02-02 16:08:03 +0800

    Feat
    adding dark and light mode toggle. Hides the unnecessary icons based on the mode. And adding each functions for toggling.

--- capstone-admin/resources/views/partials/_dnl.blade.php
+++ capstone-admin/resources/views/partials/_dnl.blade.php
@@ -1,4 +1,5 @@
 <div class="w-full flex justify-end items-center">
-    <i class="fa-solid fa-circle-half-stroke text-2xl text-primary-dark"></i>
-    <i class="fa-solid fa-circle text-2xl text-primary-dark"></i>
+    <span class="text-xs text-secondary-gray mr-3" x-text="$store.darkMode.on + ' mode'"></span>
+    <i id="dark" x-show="$store.darkMode.on === 'light'" class="fa-solid fa-circle-half-stroke text-2xl text-primary-dark"  x-data @click="$store.darkMode.toggle()"></i>
+    <i id="light"  x-show="$store.darkMode.on === 'dark'" class="fa-solid fa-sun text-2xl text-white"  x-data @click="$store.darkMode.toggle()"></i>
 </div>
