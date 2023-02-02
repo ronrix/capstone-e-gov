69194c4ca93c47ba21f5281af8aa413b2dcb5cc3
ronrix <ronrixl5@gmail.com>
2023-02-02 17:20:03 +0800

    Style
    change style for dark icon from half circle to moon

--- capstone-admin/resources/views/partials/_dnl.blade.php
+++ capstone-admin/resources/views/partials/_dnl.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full flex justify-end items-center">
     <span class="text-xs text-secondary-gray mr-3" x-text="$store.darkMode.on + ' mode'"></span>
-    <i id="dark" x-show="$store.darkMode.on === 'light'" class="fa-solid fa-circle-half-stroke text-2xl text-primary-dark"  x-data @click="$store.darkMode.toggle()"></i>
+    <i id="dark" x-show="$store.darkMode.on === 'light'" class="fa-solid fa-moon text-2xl text-primary-dark"  x-data @click="$store.darkMode.toggle()"></i>
     <i id="light"  x-show="$store.darkMode.on === 'dark'" class="fa-solid fa-sun text-2xl text-white"  x-data @click="$store.darkMode.toggle()"></i>
 </div>
