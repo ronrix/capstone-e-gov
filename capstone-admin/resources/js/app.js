69d127f3b59b46835e0c7b241a83d6fb12cb4967
ronrix <ronrixl5@gmail.com>
2023-02-02 16:04:01 +0800

    Init libraries
    Initializing jquery and alpinejs. And adding a dark mode function with alpine js

--- capstone-admin/resources/js/app.js
+++ capstone-admin/resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+import $ from "jquery";
+
+// starting alpine
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+
+localStorage.setItem("theme", JSON.stringify("dark"));
+Alpine.store('darkMode', {
+    on: JSON.parse(localStorage.getItem("theme")),
+    toggle() {
+        const theme = JSON.parse(localStorage.getItem("theme"));
+        if(theme === "dark") {
+            localStorage.setItem("theme", JSON.stringify("light"));
+            this.on = JSON.parse(localStorage.getItem("theme"));
+        }
+        else {
+            localStorage.setItem("theme", JSON.stringify("dark"));
+            this.on = JSON.parse(localStorage.getItem("theme"));
+        }
+    }
+})
+
+
+Alpine.start();
