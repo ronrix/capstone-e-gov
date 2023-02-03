8a8d6b996eb128a4f48381fbc3dd0f30b864a949
ronrix <ronrixl5@gmail.com>
2023-02-04 00:54:55 +0800

    Refactor
    Using vue for frontend. Making components and Pages and removing blade files

---
+++ capstone-admin/resources/js/Components/DarkMode.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div class="fixed right-0 top-0 p-5 flex items-center text-white">
+        <i @click="toggleDarkMode" class="fa-solid " :class="{active: darkMode, 'fa-sun' :  darkMode, 'fa-moon text-primary-dark' : !darkMode}"></i>
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from 'vue';
+
+    const darkMode = ref(false);
+
+    function toggleDarkMode() {
+        darkMode.value = !darkMode.value;
+        if(darkMode.value) {
+            document.body.classList.add("dark");
+            return;
+        }
+        document.body.classList.remove("dark")
+    }
+
+    onMounted(() => {
+        // dark mode is on
+        // if(darkMode.value) {
+        //     localStorage.setItem("darkMode", darkMode);
+        // }
+        // dark mode is off
+        console.log("the component is now mounted");
+    });
+
+</script>
---
+++ capstone-admin/resources/js/Components/Notifcation.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="fixed top-10 right-10 p-5 shadow-lg text-white" :class="{active: status >= 400 , 'bg-red-500' : status >= 200,  'bg-green-500' : status < 300 }">
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps({
+        status: Number,
+        msg: String,
+    });
+
+
+</script>
