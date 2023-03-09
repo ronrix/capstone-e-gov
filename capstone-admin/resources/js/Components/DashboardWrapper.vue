343435aacd352181268dfeca58a33ffb94dedb3c
Francisco Hinay <116621218+francishinay@users.noreply.github.com>
2023-03-09 11:20:17 +0800

    Sidebar Update (Nav links)

--- capstone-admin/resources/js/Components/DashboardWrapper.vue
+++ capstone-admin/resources/js/Components/DashboardWrapper.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="flex items-center h-screen w-screen">
+  <div class="flex items-center h-screen">
+    <!-- menu bar -->
+    <div class="block sm:hidden">
+      menu
+    </div>
     <!-- sidebar -->
-    <div class="w-[300px] h-screen">
+    <div class="h-screen hidden sm:block">
       <img src="/images/logo/black/better-pililla.png" alt="this is the logo of piililla rizal" class="mx-auto">
 
       <nav class="mt-5 flex flex-col items-start justify-start gap-5">
@@ -11,11 +15,30 @@
             <img src="/images/icons/black/gov.png" alt="this is an icon of government" class="w-[18px] mr-3">
             Government
           </a>
-          <div class="hidden pl-4">
-            <a href="">Program & Events</a>
-            <a href="">News</a>
-            <a href="">Executives</a>
-            <a href="">Job opportunities</a>
+          <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+            <a href="" class="hover:text-blue-400">News</a>
+            <a href="">Programs & Events</a>
+            <a @click="showSubNavs" class="cursor-pointer flex items-center">
+              <!-- <img src="/images/icons/black/gov.png" alt="this is an icon of government" class="w-[18px] mr-3"> -->
+              Executives
+           </a>
+           <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+              <a href="">Current Officials of Pililla Rizal</a>
+              <a href="">Former Officials of Pililla Rizal</a>
+              <a href=""> Sanggunian Barangay Officials</a>
+              <a href="">Municipal Department Heads</a>
+           </div>
+            <!-- <a href="">Executives</a> -->
+            <a href="">Job Opportunities</a>
+            <a href="">Office of the Mayor</a>
+            <a href="">Office of the Vice-Mayor</a>
+            <a href="">Barangays</a>
+            <a href="">Full Disclosure Reports</a>
+            <a href="">Map Location</a>
+            <a href="">Emergency Hotlines</a>
+            <a href="">Ordinances</a>
+            <a href="">Officials</a>
+            <a href="">Population of Pilillav</a>
           </div>
         </section>
 
@@ -26,8 +49,12 @@
             <img src="/images/icons/black/tourism.png" alt="this is an icon of government" class="w-[18px] mr-3">
             Tourism
           </a>
-          <div class="hidden pl-4">
-            <a href="">this is tourism</a>
+          <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+            <a href="">Map Location</a>
+            <a href="">Tourism</a>
+            <a href="">Tourists Spot</a>
+            <a href="">Festival</a>
+            <a href="">Guidelines</a>
           </div>
         </section>
 
@@ -37,8 +64,10 @@
             <img src="/images/icons/black/business.png" alt="this is an icon of government" class="w-[18px] mr-3">
             Businesses
           </a>
-          <div class="hidden pl-4">
-            <a href="">this is businesses</a>
+          <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+            <a href="">Apartment</a>
+            <a href="">Business Permit Form</a>
+            <a href="">Ease of Doing business</a>
           </div>
         </section>
 
@@ -48,8 +77,35 @@
             <img src="/images/icons/black/services.png" alt="this is an icon of government" class="w-[18px] mr-3">
             Services
           </a>
-          <div class="hidden pl-4">
-            <a href="">this is services</a>
+          <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+            <a @click="showSubNavs" class="cursor-pointer flex items-center">
+            <img src="/images/icons/black/services.png" alt="this is an icon of government" class="w-[18px] mr-3">
+            Office of the Mayor
+          </a>
+            <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+              <a href="">Example services</a>
+            </div>
+            <a @click="showSubNavs" class="cursor-pointer flex items-center">
+            <img src="/images/icons/black/services.png" alt="this is an icon of government" class="w-[18px] mr-3">
+            MSWD Department
+          </a>
+            <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+              <a href="">Example services</a>
+            </div>
+            <a @click="showSubNavs" class="cursor-pointer flex items-center">
+            <img src="/images/icons/black/services.png" alt="this is an icon of government" class="w-[18px] mr-3">
+            Agriculture Department
+          </a>
+            <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+              <a href="">Example services</a>
+            </div>
+            <a @click="showSubNavs" class="cursor-pointer flex items-center">
+            <img src="/images/icons/black/services.png" alt="this is an icon of government" class="w-[18px] mr-3">
+            HR Department
+          </a>
+            <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+              <a href="">Example services</a>
+            </div>
           </div>
         </section>
 
@@ -59,16 +115,21 @@
             <img src="/images/icons/black/about.png" alt="this is an icon of government" class="w-[18px] mr-3">
             About
           </a>
-          <div class="hidden pl-4">
-            <a href="">this is about</a>
+          <div class="hidden flex mt-2 pl-4 flex-col items-start justify-start gap-2 border border-l-4 ml-5 border-r-0 border-b-0 border-t-0">
+            <a href="">Mission & Vission</a>
+            <a href="">History</a>
+            <a href="">Location</a>
+            <a href="">Official Seal</a>
+            <a href="">Website Developers</a>
+            <a href="">Conatacts</a>
           </div>
         </section>
 
       </nav>
     </div>
-    <div class="w-full h-full">
+    <div class="flex-1 h-full relative">
       <!-- header -->
-      <div class="h-[30px] w-full flex justify-between items-center">
+      <div class="h-[30px] flex justify-between items-center">
         <h3>government &hookrightarrow; news</h3>
         <button @click="alertMe" class="px-5 font-bold text-sm">Add new</button>
       </div>
