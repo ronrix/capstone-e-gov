baba3eec45d42af005987927b9ceea80753fa778
ronrix <medyochain@gmail.com>
2023-03-10 23:36:02 +0800

    adding news and job postings

--- capstone-admin/resources/js/Pages/Dashboard/Dashboard.vue
+++ capstone-admin/resources/js/Pages/Dashboard/Dashboard.vue
@@ -6,7 +6,23 @@
             <Card title="Total viewers" borderColor="#000" amount="600" />
         </div>
 
-        <h3 class="font-bold text-xl mt-5">Programs & Events</h3>
+        <!-- 
+            display programs showing how many comments 
+            when clicked it should display a modal displaying all the comments and the admin acan also add their comments
+        -->
+        <div class="mt-14">
+            <h3 class="font-bold text-xl">Programs & Events</h3>
+            <HorizontalCard commentsCount="100" title="Happy fiesta Pililla Rizal" imgSrc="https://images.unsplash.com/photo-1554037895-b5ec1389b928?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" />
+        </div>
+        <div class=" mt-5">
+            <h3 class="font-bold text-xl">News</h3>
+            <HorizontalCard commentsCount="23" title="Coffee na masaarap sa Oval, di daw masarap" imgSrc="https://images.unsplash.com/photo-1525480038216-45653b3aac92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+        </div>
+        <div class=" mt-5">
+            <h3 class="font-bold text-xl">Job Postings</h3>
+            <HorizontalCard commentsCount="23" title="Looking for viva max artists" imgSrc="https://images.unsplash.com/photo-1500628550463-c8881a54d4d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" />
+        </div>
+
         <!-- <div class="m-5 border p-3 shadow-md rounded-lg">
             <h4 class="font-bold text-xl">Content Title</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sit sapiente, veritatis commodi rerum
@@ -25,6 +41,7 @@
 import { onMounted } from "vue";
 import Test from "../Test.vue";
 import Card from "./Card.vue";
+import HorizontalCard from "./HorizontalCard.vue";
 
 onMounted(() => {
     document.title = "Better Pililla | Dashboard";
