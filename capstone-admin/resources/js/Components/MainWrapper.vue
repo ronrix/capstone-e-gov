ec620f011a35fa8482b5185946fae88631f0a01d
ronrix <medyochain@gmail.com>
2023-03-11 10:50:50 +0800

    style head. adding icons and avatar

--- capstone-admin/resources/js/Components/MainWrapper.vue
+++ capstone-admin/resources/js/Components/MainWrapper.vue
@@ -126,11 +126,32 @@
 
       </nav>
     </div>
-    <div class="flex-1 h-full relative p-2">
+    <div class="flex-1 h-full relative p-5">
       <!-- header -->
       <div class="h-[30px] flex justify-between items-center">
-        <h3>{{ currentRoute }}</h3>
-        <button @click="showAddModal" class="px-5 font-bold text-sm">Add new</button>
+        <h3 class="font-bold text-sm"> you are in  <span class="text-blue-600">{{ currentRoute }}</span></h3>
+        <div class="flex items-center justify-center">
+          <div class="relative flex mr-5">
+            <i class="uil uil-bell text-xl text-gray-700 hover:text-slate-500"></i>
+            <div class="absolute -right-2 -top-3">
+              <span class="relative flex h-3 w-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+              </span>
+            </div>
+          </div>
+          <div class="relative flex mr-5">
+            <i class="uil uil-comment-alt-message text-xl text-gray-700 hover:text-slate-500"></i>
+            <div class="absolute -right-2 -top-3">
+              <span class="relative flex h-3 w-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+              </span>
+            </div>
+          </div>
+          <img class="w-[30px] rounded-full" src="https://sm.ign.com/ign_ap/cover/a/avatar-gen/avatar-generations_hugw.jpg" alt="">
+          <i class="uil uil-angle-down text-gray-700 text-2xl"></i>
+        </div>
       </div>
       <slot></slot>
     </div>
