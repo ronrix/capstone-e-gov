a86ab2d5535882428564b01bbfc005e38d7e30a3
ronrix <medyochain@gmail.com>
2023-03-12 22:31:36 +0800

    add news design and modal with no design yet

--- capstone-admin/resources/js/Pages/Government/News/News.vue
+++ capstone-admin/resources/js/Pages/Government/News/News.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    this is the news page
+  <div class="mt-5 p-3 flex flex-col gap-5">
+    <CardNews :showNewsModal="showNewsModal" title="news first" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?" />
+    <CardNews :showNewsModal="showNewsModal" title="news first" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?" />
+    <CardNews :showNewsModal="showNewsModal" title="news first" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?" />
+
+    <NewsModal :showNewsModal="showNewsModal" v-show="isNewModal" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import CardNews from './CardNews.vue';
+import NewsModal from './NewsModal.vue';
+
+import { ref } from 'vue';
+const isNewModal = ref(false);
+
+function showNewsModal() {
+  console.log("you clicked me");
+  isNewModal.value = !isNewModal.value;
+}
+
+</script>
 
 <style lang="scss" scoped>
 
---
+++ capstone-admin/resources/js/Pages/Government/News/NewsModal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 bg-black/20 flex items-center justify-center">
+    <div class="bg-white w-[700px] p-5 rounded-lg relative">
+      <div @click="showNewsModal" class="m-0 p-0 cursor-pointer">
+        <i class="uil uil-times text-gray-800 hover:text-blue-800 text-xl absolute top-0 right-2 p-2"></i>
+      </div>
+      <h4 class="font-bold text-2xl">this is the news modal</h4>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  showNewsModal: Function
+})
+</script>
