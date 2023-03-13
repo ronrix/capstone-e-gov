1f51c1e7583e039b6744440909e917780bb7c5a3
Arce007 <124360507+Arce007@users.noreply.github.com>
2023-03-14 00:07:19 +0800

    Muningning.
    Done nothing, hehe

--- capstone-admin/resources/js/Pages/Businesses/Apartments/Apartments.vue
+++ capstone-admin/resources/js/Pages/Businesses/Apartments/Apartments.vue
@@ -3,7 +3,7 @@
     <h1 class="font-bold text-xl">Apartments</h1>
     <div class="flex">
       <div class=" p-3 border w-sm mx-auto hover:bg-white shadow-md overflow-hidden md:max-w-sm">
-        <img classs="h-15 w-sm object-cover md:h- md:w-48" src="https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg" alt="A person that is planning">
+        <img classs="h-15 w-sm object-cover md:h- md:w-48" src="https://www.apartments.com/images/librariesprovider2/blank-images/parkline-apartment-in-miami-flba486679-f59b-475d-885a-ae52659d1e51.jpg?sfvrsn=264e5d72_1" alt="a creamy white theme house">
         <p class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">type of apartment</p>
         <h3>Apartment's name</h3>
         <p>Location</p>
