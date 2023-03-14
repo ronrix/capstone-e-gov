994d13e38ef992af31a6b45b73a24d3e9ff39fd4
Arce007 <124360507+Arce007@users.noreply.github.com>
2023-03-14 18:28:34 +0800

    PLease check mine
    @ronrix

--- capstone-admin/resources/js/Pages/Businesses/Apartments/Apartments.vue
+++ capstone-admin/resources/js/Pages/Businesses/Apartments/Apartments.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <WrapperContent>
+    <!--Remember to add filter for both type and title-->
+
+    <CardApartment v-for="data in searchFilter()" :data="data" :showApartmentModal="showApartmentModal" />
+    <ApartmentModal :selectedData="selectedData" :showApartmentModal="showApartmentModal" v-if="isNewModal" />
+    
+    <button @click="showApartmentModal" class="rounded-full shadow-md p-4 bg-blue-600 flex items-center justify-center w-[50px] h-[50px] fixed bottom-5 right-5 hover:bg-blue-500">
+      <i class="uil uil-plus text 2xl text-white"></i>
+    </button>
+
+    <AddModal :showAddModal="showApartmentModal" :isAddModal="isAddNewModal" v-if="isAssNewModal" />
+    </WrapperContent>
+  <!--
+    <div>
     <h1 class="font-bold text-xl">Apartments</h1>
     <div class="flex flex-row md:flex-row">
       <div class="items-start flex-row p-3 border w-sm mx-auto hover:bg-white shadow-md overflow-hidden md:max-w-sm">
@@ -15,12 +28,71 @@
         <p>Location</p>
       </div>
       <div class="p-3 border w-sm mx-auto hover:bg-white shadow-md overflow-hidden md:max-w-sm">
-        <img classs="h-sm w-sm object-cover md:h-10 md:w-48" src="https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg" alt="A person that is planning">
+        <img classs="h-smcd w-sm object-cover md:h-10 md:w-48" src="https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg" alt="A person that is planning">
         <p class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">type of apartment</p>
         <h3>Apartment's name</h3>
         <p>Location</p>
       </div>
       
     </div>
-  </div>
+  </div> 
+  -->
 </template>
+
+<script setup>
+import CardApartment from './CardApartment.vue';
+import ApartmentModal from './ApartmentModal.vue';
+
+
+import { ref, onUpdated } from 'vue';
+import AddModal from '../../../Components/AddModal/AddModal.vue'
+const isNewModal = ref(false)
+const selectedData = ref();
+
+const isAddNewModal = ref(false);
+function showAddNewModal() {
+  isAddNewModal.value = !isAddNewModal.value;
+}
+const sample_data = [
+  {
+    title:"Aparment #1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
+    img:"https://www.apartments.com/images/librariesprovider2/blank-images/parkline-apartment-in-miami-flba486679-f59b-475d-885a-ae52659d1e51.jpg?sfvrsn=264e5d72_1",
+},
+{
+    title:"Aparment #2",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
+    img:"https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg"
+},
+{
+    title:"Aparment #3",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
+    img:"https://www.apartments.com/images/librariesprovider2/blank-images/parkline-apartment-in-miami-flba486679-f59b-475d-885a-ae52659d1e51.jpg?sfvrsn=264e5d72_1",
+},
+{
+    title:"Aparment #4",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
+    img:"https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg"
+},
+];
+
+function showApartmentModal(data){
+  isNewModal.value = !isNewModal.value;
+  selectedData.value = data;
+}
+// Should Add data filtering search input
+
+
+onUpdated(() => {
+  if(isNewModal.value || isNewModal.value) {
+    document.body.classList.add('overflow-hidden');
+  }
+  else {
+    document.body.classList.remove('overflow-hidden');
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ capstone-admin/resources/js/Pages/Businesses/Apartments/CardApartment.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-row items-start shadow-lg bg-white rounded-lg">
+        <!---Image-->
+        <div :style="{backgroundImage: `url(${data.img})`}" class="bg-no-repeat bg-cover bg-center h-[300px] w-full md:w-[300px] lg:w-[500px] relative after:h-[50px] after:bottom-0 after:w-full after:bg-gradient-to-t after:from-black/30 after:to-white/10 after:absolute after:blur-sm">
+        </div>
+        <!--content-->
+        <div class="flex-1 p-5 flex flex-c items-start justify-between"> 
+            <h3 class="font-bold uppercase text-xl tracking-wide">{{ data.title }}</h3>
+            <h5 class="text-xs text-gray-500 font-bold">{{ data.type }}</h5>
+            <p class="mt-5 text-ellipsis text-sm h-[100px] overflow-hidden">
+            {{ data.content }}
+            </p>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+  showNewsModal: Function,
+  data: Object
+});
+</script>
