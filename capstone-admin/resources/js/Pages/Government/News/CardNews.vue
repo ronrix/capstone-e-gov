97921a2dcaf0cd10d6f8e2d5794b8923627198bb
ronrix <medyochain@gmail.com>
2023-03-15 09:49:25 +0800

    adding delete modal to cardNews

--- capstone-admin/resources/js/Pages/Government/News/CardNews.vue
+++ capstone-admin/resources/js/Pages/Government/News/CardNews.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="flex flex-col md:flex-row items-start shadow-lg bg-white border border-r-0 border-y-0 border-l-[5px] border-l-blue-600 ">
+  <div class="relative overflow-hidden flex flex-col md:flex-row items-start shadow-lg bg-white border border-r-0 border-y-0 border-l-[5px] border-l-blue-600 ">
+ 
+    <!-- delete button this will delete the programs and events --> 
+    <div class="z-10"> <!-- adding z-10 to put this delete component at the top/front -->
+      <Delete />
+    </div>
+
     <!-- image -->
     <div :style="{ backgroundImage: `url(${data.img})` }" class="bg-no-repeat bg-cover bg-center h-[300px] w-full md:w-[300px] lg:w-[500px] relative after:h-[50px] after:bottom-0 after:w-full after:bg-gradient-to-t after:from-black/30 after:to-white/10 after:absolute after:blur-sm after:block md:after:hidden "> </div>
     <!-- content -->
@@ -9,14 +15,16 @@
       <p class="mt-5 text-ellipsis text-sm h-[100px] overflow-hidden">
         {{ data.content }}
       </p>
-      <button @click="showNewsModal(data)" class="bg-blue-600 hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs self-end mt-5 uppercase">view </button>
+      <button @click="showPreviewModal(data)" class="bg-blue-600 hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs self-end mt-5 uppercase">view </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import Delete from '../../../Components/Delete.vue';
+
 defineProps({
-  showNewsModal: Function,
+  showPreviewModal: Function,
   data: Object
 });
 </script>
