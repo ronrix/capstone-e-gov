44e5465db39eb410a622e2c71671569c0162d7cb
ronrix <medyochain@gmail.com>
2023-03-16 23:09:53 +0800

    adding hotline modal to handle form for creating new hotline number and i also added a sorting functionality with deparment property toggling alphabettically

--- capstone-admin/resources/js/Pages/Government/Hotlines/Hotlines.vue
+++ capstone-admin/resources/js/Pages/Government/Hotlines/Hotlines.vue
@@ -2,17 +2,20 @@
   <WrapperContent>
     <h3 class="font-bold text-xl mb-3">Municipal Hotlines</h3>
     <!-- hotlines table -->
-    <div class="w-full overflow-x-auto">
+    <div class="w-full flex flex-col">
       <table class="text-left border-separate w-full">
         <thead>
           <tr class="bg-gray-200 text-xs md:text-sm">
-            <th>Departments</th>
+            <th class="flex items-center justify-between">
+              Departments
+              <i @click="sortFn" class="uil uil-sorting cursor-pointer hover:text-blue-600 text-lg"></i>
+            </th>
             <th>Numbers</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="a in hotlines" class="text-xs md:text-sm font-medium">
+          <tr v-for="a in filteredData" class="text-xs md:text-sm font-medium">
             <td class="bg-white">
               <p @click="startEditing" class="m-0 p-0">
                 {{ a.department }}
@@ -27,17 +30,27 @@
               </p>
               <textarea @blur="stopEditing" v-model="a.number" class="hidden w-auto overflow-scroll no-scrollbar m-0 text-sm font-bold h-[20px] max-h-[20ppx]">{{ a.number }}</textarea>
             </td>
-            <td class="bg-white text-center"><button class="bg-red-600 text-white px-3 text-sm font-bold rounded-md">delete</button></td>
+            <td class="bg-white text-center"><button class="border border-red-600 text-red-600 hover:bg-red-600 hover:text-white px-3 text-sm font-bold rounded-md">delete</button></td>
           </tr>
         </tbody>
       </table>
+      <!-- add btn -->
+      <button @click="showHotlineModal" class="border border-blue-600 text-blue-600 hover:bg-blue-500 hover:text-white self-end mt-5 flex items-center justify-center rounded-lg px-5 uppercase text-md font-bold">
+        new
+        <i class="uil uil-plus-circle m-0 ml-2"></i>
+      </button>
     </div>
+
+    <!-- hotline modal -->
+    <HotlineModal v-if="isHotlineModal" :showHotlineModal="showHotlineModal" :sample_data="sample_data" />
 
   </WrapperContent>
 </template>
 
 <script setup>
 import { startEditing, stopEditing } from '../../../utils/editFn';
+import HotlineModal from "./HotlineModal.vue";
+import { ref } from 'vue';
 
 const hotlines = [
   {
@@ -57,6 +70,48 @@ const hotlines = [
     number: "091290848",
   },
 ]
+const isSortedIncrease = ref(true);
+const filteredData = ref(hotlines);
+
+const isHotlineModal = ref(false);
+function showHotlineModal() {
+  isHotlineModal.value = !isHotlineModal.value;
+}
+
+function sortFn() {
+  isSortedIncrease.value = !isSortedIncrease.value;
+
+  // incresing
+  if(isSortedIncrease.value) {
+    filteredData.value.sort((a, b) => {
+      let fa = a.department.toLowerCase(),
+      fb = b.department.toLowerCase();
+
+      if (fa < fb) {
+          return -1;
+      }
+      if (fa > fb) {
+          return 1;
+      }
+      return 0;
+    });
+
+    return;
+  }
+  // decreasing
+  filteredData.value.sort((a, b) => {
+    let fa = a.department.toLowerCase(),
+    fb = b.department.toLowerCase();
+
+    if (fa > fb) {
+        return -1;
+    }
+    if (fa < fb) {
+        return 1;
+    }
+    return 0;
+  });
+}
 
 </script>
 
