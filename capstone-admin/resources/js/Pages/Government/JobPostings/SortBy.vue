d319de33d477f773c3cc1735835f2f55a426d3d6
ronrix <medyochain@gmail.com>
2023-03-16 13:43:23 +0800

    adding sort function and add a seperate sort component

---
+++ capstone-admin/resources/js/Pages/Government/JobPostings/SortBy.vue
@@ -0,0 +1,18 @@
+<template>
+  <label class="text-sm ml-5">
+    <span class="text-gray-500 font-bold">
+      Filter by:
+    </span>
+    <select :value="sortBy" @change="sortFn" class="bg-white px-3 font-bold rounded-md">
+      <option v-for="sortCat in sortCategories" :value="sortCat">{{ sortCat }}</option>
+    </select>
+  </label>
+</template>
+
+<script setup>
+defineProps({
+  sortBy: String,
+  sortFn: Function,
+  sortCategories: Array
+})
+</script>
