6ad71e7d033a797869134e5c2903d38c62161b34
ronrix <medyochain@gmail.com>
2023-03-18 01:00:34 +0800

    adding executives pages

---
+++ capstone-admin/resources/js/Pages/Government/Executives/CardOfficial.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col bg-white rounded-md p-3 shadow-md relative overflow-hidden">
+    <!-- delete -->
+    <Delete new_class="md:translate-x-[90%]" />
+
+    <img class="h-[250px]" :src="official.img" :alt="official.name">
+
+    <h5 class="text-gray-500 text-base font-bold uppercase">{{ official.position }}</h5>
+    <h3 class="text-gray-800 text-2xl font-bold capitalize">{{ official.name }}</h3>
+
+  </div>
+</template>
+
+<script setup>
+import Delete from '../../../Components/Delete.vue';
+
+defineProps({
+  official: Object
+})
+</script>
