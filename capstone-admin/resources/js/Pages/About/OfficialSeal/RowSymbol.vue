a9ab2c1c58a9ef93b9825b9cb4572eb1b4936d34
Francisco Hinay <116621218+francishinay@users.noreply.github.com>
2023-03-17 17:31:35 +0800

    Added About page (Official Seal)

---
+++ capstone-admin/resources/js/Pages/About/OfficialSeal/RowSymbol.vue
@@ -0,0 +1,36 @@
+<template>
+     <!-- table 2 -->
+    <tbody>
+        <tr class="text-xs md:text-sm lg:text-md">
+            <td class="text-center"><textarea
+                    class="w-full h-[90px] max-h-[90px] focus:outline-blue-500 py-1 px-4 overflow-scroll no-scrollbar bg-white">{{ seal1?.symbol }}</textarea>
+            </td>
+            <td class="text-center"><textarea
+                    class="w-full h-[90px] max-h-[90px] focus:outline-blue-500 py-1 px-4 overflow-scroll no-scrollbar bg-white">{{ seal1?.description1 }}</textarea>
+            </td>
+        </tr>
+    </tbody>
+</template>
+
+<script setup>
+defineProps({seal1: Object,})
+</script>
+
+<style scoped>
+table,
+th,
+td {
+  border: 1px solid gray;
+}
+
+table {
+  background-color: white;
+}
+
+td {
+  padding: 0.7em;
+}
+th{
+    padding: 0.2em;
+}
+</style>
