53a8f0fb7e787f88e73d3cae1896fc6019da13e5
Francisco Hinay <116621218+francishinay@users.noreply.github.com>
2023-03-18 01:01:45 +0800

    Added Ease of Doing Business

--- capstone-admin/resources/js/Pages/Businesses/EaseOfDoingBusiness/EaseOfDoingBusiness.vue
+++ capstone-admin/resources/js/Pages/Businesses/EaseOfDoingBusiness/EaseOfDoingBusiness.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
+  <WrapperContent>
+    <h1 class="text-2xl font-bold text-gray-700 mb-5">Ease of Doing Business</h1>
 
-  </div>
+    <!-- first card -->
+
+    <div class="flex flex-row bg-white rounded-lg p-0">
+      <div class="flex-1 justify-between p-10"> 
+          <p class="text-sm font-medium text-gray-800">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+        <button class="flex-1 bg-blue-600  hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs  mt-10 uppercase">view </button>
+      </div>
+
+      <!-- image -->
+
+      <div class="flex-1">
+        <img src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600" alt="">
+      </div>
+    </div>
+
+      <!-- second-card -->
+
+     
+
+  </WrapperContent>
 </template>
 
 <script setup>
-
+import WrapperContent from '../../../Components/WrapperContent.vue';
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
