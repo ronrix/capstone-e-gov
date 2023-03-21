7d98b73f143631025075c27428bec430bb721726
ronrix <medyochain@gmail.com>
2023-03-21 10:00:31 +0800

    adding filter by date functionality and removing the sample data variable

--- capstone-admin/resources/js/Pages/Government/News/News.vue
+++ capstone-admin/resources/js/Pages/Government/News/News.vue
@@ -7,6 +7,20 @@
         <i class="uil uil-search-alt pr-3"></i>
         <input type="search" v-model.lazy="search" placeholder="search news" class="bg-transparent outline-none" />
       </label> 
+
+      <!-- filter by date -->
+      <!-- month -->
+      <span class="text-gray-500 font-bold text-sm mx-2 capitalize">
+        month:
+      </span>
+      <select id="month" @change="filterBy" :value="filterMonth" class="bg-white px-3 font-bold rounded-md text-gray-600 text-sm">
+        <option v-for="month in filterMonths" :value="month">{{ month }}</option>
+      </select>
+      <!-- year -->
+      <span class="text-gray-500 font-bold text-sm mx-2 capitalize">
+        year:
+      </span>
+      <input id="year" @change="filterBy" :value="filterYear" type="number" min="2000" :max="new Date().getFullYear()" class="text-sm text-gray-600 font-bold px-2">
     </div>
 
     <!-- news card -->
@@ -16,6 +30,8 @@
         <div class="flex-1 border"></div>
       </div>
       <CardNews v-for="data in group.items" :data="data" :showPreviewModal="showPreviewModal" />
+      <!-- no result  -->
+      <div v-if="!group.items.length" class="font-bold text-gray-800">No results found!</div>
     </div>
     <PreviewModal :selectedData="selectedData" :showPreviewModal="showPreviewModal" v-if="isPreviewModal" />
 
@@ -33,14 +49,60 @@
 import CardNews from './CardNews.vue';
 import PreviewModal from '../../../Components/PreviewModal.vue';
 
-import { ref, onUpdated, computed, onMounted } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import AddModal from '../../../Components/AddModal/AddModal.vue';
 import { searchFilter } from '../../../utils/searchFilter';
 import { be_url } from "../../../config/config";
+import { dateFormat } from '../../../utils/dateFormat';
 
-const isPreviewModal = ref(false);
-const selectedData = ref();
-const dataNews = ref([]);
+
+// sort/filter function for select and input year
+const filterMonth = ref("All");
+const filterYear= ref(new Date().getFullYear());
+const filterMonths = ["All", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function filterBy(e) {
+  if(e.target.id === "month") {
+    filterMonth.value = e.target.value;
+
+    // if value is "all" select all the data
+    if(e.target.value === "All") {
+      dataNews.value = originalDataNews.value;
+      return;
+    }
+
+    // update the value of filterMonth with the selected one
+    filterMonth.value = e.target.value;
+
+    // filter by month
+    // and set the new value to dataNews to re-render the filtered news
+    const groups = originalDataNews.value.filter(group => {
+      const date = dateFormat(group.created_at);
+      // return the group if month exists 
+      return date.toLocaleLowerCase().includes(filterMonth.value.toLowerCase());
+    }); 
+    dataNews.value = groups;
+  }
+  else {
+    // update the value of filterYear selected one
+    filterYear.value = e.target.value;
+
+    // filter by year
+    // and set the new value to dataNews to re-render the filtered news
+    const groups = originalDataNews.value.filter(group => {
+      const date = dateFormat(group.created_at);
+      // return the group if month exists 
+      return date.toLocaleLowerCase().includes(filterYear.value.toLowerCase());
+    }); 
+    dataNews.value = groups;
+  }
+ 
+}
+
+const isPreviewModal = ref(false); // this state is for toggling/showing preview modal
+const selectedData = ref(); // this state for vieing the data in the preview modal
+const dataNews = ref([]); // this state is going to be used for modifying like filter or search function
+const originalDataNews = ref([]); // this state will act as the original data that don't change
 
 const isAddNewModal = ref(false);
 function showAddNewModal() {
@@ -51,36 +113,10 @@ function showAddNewModal() {
 onMounted(() => {
   // requesting data from /news
   fetch(be_url + "/news").then((res) => res.json()).then(data => {
-      dataNews.value = data; 
+      dataNews.value = data.news; 
+      originalDataNews.value = data.news; 
   });
 });
-
-const sample_data = [
-  {
-    title: "First news",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
-    img: "https://images.unsplash.com/photo-1557992260-ec58e38d363c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    date: "2022-01-01",
-  },
-  {
-    title: "Second news",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
-    img: "https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    date: "2020-05-01",
-  },
-  {
-    title: "third news",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
-    img: "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    date: "2021-12-01",
-  },
-  {
-    title: "fourth news",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsam labore, laborum neque praesentium ullam eos quam eaque laboriosam expedita, ut dicta veritatis rerum molestiae, nemo quod accusantium repellendus corrupti?",
-    img: "https://images.unsplash.com/photo-1526470608268-f674ce90ebd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-    date: "2023-02-01",
-  },
-];
 
 function showPreviewModal(data) {
   isPreviewModal.value = !isPreviewModal.value;
@@ -92,7 +128,8 @@ const search = ref("");
 const groupData = computed(() => {
   let groups = {};
 
-  dataNews?.value?.news?.forEach((item) => {
+  // this will group data by month
+  dataNews?.value?.forEach((item) => {
     const month = new Date(item.created_at).toLocaleString('default', { month: 'short', year: "numeric" });
     // group item by month
     if (!groups[month]) {
@@ -101,38 +138,41 @@ const groupData = computed(() => {
     groups[month].items.push(item);
   });
 
+  // converting the object to array of objects;
+  // from {jan: [], feb: []} -> [{jan: [], feb: []}]
+  // to make it iterable
   groups = Object.values(groups);
 
+  // after making the 'groups' iterable we can now sort them by date
+  // latest -> oldest
   groups.sort((a, b) => {
     const dateA = new Date(a.items[0].date);
     const dateB = new Date(b.items[0].date);
     return dateB - dateA;
   });
 
+  const newGroup = convertToObjectArray(groups);
+  return newGroup;
+}, [dataNews]);
+
+// then this function will turn the array of objects to an object of array
+// from [{jan: [], feb: []}] -> {jan: [], feb: []} 
+function convertToObjectArray(groups) {
   return groups.reduce((acc, group) => {
     acc[group.month] = group;
     return acc;
   }, {});
-}, [dataNews]);
+}
 
 const searchData = ref([{ month: "Search Results", items: []}]);
 const dataToLoop = computed(() => {
   if(search.value.length) {
-    searchData.value[0].items = searchFilter(dataNews.value?.news, search);
+    searchData.value[0].items = searchFilter(dataNews.value, search);
     return searchData;
   }
   return groupData;
-}, [search]);
+}, [search, filterMonth, filterYear]);
 
-// check if modal is open and prevent the document from scrolling
-onUpdated(() => {
-  if(isPreviewModal.value || isAddNewModal.value) {
-    document.body.classList.add('overflow-hidden');
-  }
-  else {
-    document.body.classList.remove('overflow-hidden');
-  }
-});
 
 </script>
 
