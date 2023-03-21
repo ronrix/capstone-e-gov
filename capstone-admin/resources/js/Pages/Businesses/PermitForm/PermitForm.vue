4eb6029d43243991dee9128ea83552fe24a83fba
Arce007 <124360507+Arce007@users.noreply.github.com>
2023-03-21 18:14:32 +0800

    ,
    ,,,

--- capstone-admin/resources/js/Pages/Businesses/PermitForm/PermitForm.vue
+++ capstone-admin/resources/js/Pages/Businesses/PermitForm/PermitForm.vue
@@ -1,33 +1,38 @@
 <template>
-  <HeadTitle title="BPLO"></HeadTitle>
+  <HeadTitle title="Business Permit"></HeadTitle>
   <div>
    <WrapperContent>
-    <h3 class="font-bold text-xl mb-3 text-gray-800">Checklist of Requiremnts</h3>
-        <WrapperReq>
+    <h3 class="font-bold text-xl mb-3 text-gray-800">Checklist of Requirements</h3>
+        <PwrapperReq>
             <div>
-                <ListReq title="Passport" />
-                <ListReq title="Voter's ID" />
-                <ListReq title="TIN ID" />
-                <ListReq title="Philhealth ID" />
-                <ListReq title="SSS ID" />
-                <ListReq title="UMID ID" />
-                <ListReq title="Driver's License" />
-                <ListReq title="Other Government ID" />
+                <PListReq title="Passport" />
+                <PListReq title="Voter's ID" />
+                <PListReq title="TIN ID" />
+                <PListReq title="Philhealth ID" />
+                <PListReq title="SSS ID" />
+                <PListReq title="UMID ID" />
+                <PListReq title="Driver's License" />
+                <PListReq title="Other Government ID" />
             </div>
             <div>
-                <ListReq title="PRC ID" />
-                <ListReq title="GSIS E-card" />
-                <ListReq title="OFW ID" />
-                <ListReq title="OWWA ID" />
-                <ListReq title="Seamans Book" />
-                <ListReq title="Postal ID" />
+                <PListReq title="PRC ID" />
+                <PListReq title="GSIS E-card" />
+                <PListReq title="OFW ID" />
+                <PListReq title="OWWA ID" />
+                <PListReq title="Seamans Book" />
+                <PListReq title="Postal ID" />
             </div>
-         </WrapperReq>
+         </PwrapperReq>
         </WrapperContent>
   </div>
 </template>
 
 <script setup>
+import PListReq from './PermitFormComponents/PListReq.vue';
+import WrapperContent from '../../../Components/WrapperContent.vue';
+import PwrapperReq from 
+
+
 
 </script>
 
