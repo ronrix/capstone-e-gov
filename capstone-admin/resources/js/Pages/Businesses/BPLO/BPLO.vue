addfb7bf67a3a9404835e45f39d9d1ca0e5f863d
Arce007 <124360507+Arce007@users.noreply.github.com>
2023-03-23 01:13:44 +0800

    BPLO-added ListOfRqrmnts, INDEX changed components and name
    apparently I have committed this, but I can't remember right so Im committing it again

--- capstone-admin/resources/js/Pages/Businesses/BPLO/BPLO.vue
+++ capstone-admin/resources/js/Pages/Businesses/BPLO/BPLO.vue
@@ -4,26 +4,20 @@
    <WrapperContent>
     <h3 class="font-bold text-xl mb-3 text-gray-800">Checklist of Requirements</h3>
         <PwrapperReq>
-            <div>
+            <div class="flex-wrap">
                 <PListReq title="1. Application Form (3 copies)" />
                 <PListReq title="1.1 Location Sketch" />
                 <PListReq title="1.2 2x2 formal picture of logo of business" />
                 <PListReq title="1.3 3R size picture of front and exterior of the establishment" />
                 <PListReq title="2. Barangay Clearance" />
-                <PListReq title="SEC Certificate (Article and Bi-laws) or CDA registration" />
-                <PListReq title="Lesson permit and copy of Lease agreement/Lease of Contact or Affidavit of concent" />
-                <PListReq title="Insurance Policy or Official receipt of Comprehensive General Liability (CGL)" />
-                <PListReq title="Insurance Policy or Official receipt of Comprehensive General Liability (CGL)" />
-                <PListReq title="Insurance Policy or Official receipt of Comprehensive General Liability (CGL)" />
+                <PListReq title="3. SEC Certificate (Article and Bi-laws) or CDA registration" />
+                <PListReq title="4. Lesson permit and copy of Lease agreement/Lease of Contact or Affidavit of concent from the Lessor if leasing" />
+                <PListReq title="5. Insurance Policy or Official receipt of Comprehensive General Liability (CGL)" />
+                <PListReq title="6. Zoning Clearance & Location Clearance" />
+                <PListReq title="7. Fire Safety Inspection Certificate" />
+                <PListReq title="9. Latest RPT Payment Receipt" />
             </div>
-            <div>
-                <PListReq title="PRC ID" />
-                <PListReq title="GSIS E-card" />
-                <PListReq title="OFW ID" />
-                <PListReq title="OWWA ID" />
-                <PListReq title="Seamans Book" />
-                <PListReq title="Postal ID" />
-            </div>
+
          </PwrapperReq>
         </WrapperContent>
   </div>
