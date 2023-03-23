848baf345d72addd04332e9cb5f9d8fdb92d8a42
Arce007 <124360507+Arce007@users.noreply.github.com>
2023-03-23 12:14:39 +0800

    Update Apartments.vue

--- capstone-admin/resources/js/Pages/Businesses/Apartments/Apartments.vue
+++ capstone-admin/resources/js/Pages/Businesses/Apartments/Apartments.vue
@@ -18,32 +18,6 @@
 
     <AddModal :showAddModal="showAddModal" :isAddModal="isAddNewModal" v-if="isAddNewModal" />
     </WrapperContent>
-  <!--
-    <div>
-    <h1 class="font-bold text-xl">Apartments</h1>
-    <div class="flex flex-row md:flex-row">
-      <div class="items-start flex-row p-3 border w-sm mx-auto hover:bg-white shadow-md overflow-hidden md:max-w-sm">
-        <img classs="h-sm w-sm object-cover md:h-10 md:w-48" src="https://www.apartments.com/images/librariesprovider2/blank-images/parkline-apartment-in-miami-flba486679-f59b-475d-885a-ae52659d1e51.jpg?sfvrsn=264e5d72_1" alt="a creamy white theme house">
-        <p class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">type of apartment</p>
-        <h3>Apartment's name</h3>
-        <p>Location</p>
-      </div>
-      <div class="items-start md:flex-row p-3 border w-sm mx-auto hover:bg-white shadow-md overflow-hidden md:max-w-sm">
-        <img classs="h-sm w-sm object-cover md:h-10 md:w-48" src="https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg" alt="A person that is planning">
-        <p class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">type of apartment</p>
-        <h3>Apartment's name</h3>
-        <p>Location</p>
-      </div>
-      <div class="p-3 border w-sm mx-auto hover:bg-white shadow-md overflow-hidden md:max-w-sm">
-        <img classs="h-smcd w-sm object-cover md:h-10 md:w-48" src="https://images.ctfassets.net/pdf29us7flmy/30jdDV7ig6aoq8JR1G8NSP/02ba1881d2bdfcef85e3dc7c10ced839/-IND-001-017-_How_To_Create_a_Successful_Brand_Positioning_Strategy_Final__1_.jpg" alt="A person that is planning">
-        <p class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">type of apartment</p>
-        <h3>Apartment's name</h3>
-        <p>Location</p>
-      </div>
-      
-    </div>
-  </div> 
-  -->
 </template>
 
 <script setup>
