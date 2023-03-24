8c391ff8145b5b601f24fa8f1cc51aa26544f504
ronrix <127171334+Sazurc@users.noreply.github.com>
2023-03-24 17:51:00 +0800

    Update TouristSpot.vue

--- capstone-admin/resources/js/Pages/Tourism/TouristSpot.vue
+++ capstone-admin/resources/js/Pages/Tourism/TouristSpot.vue
@@ -4,8 +4,14 @@
         <!-- search filter -->
         <SearchInput class="mb-5" @searchFn="searchFn" />
 
+        <div v-for="group in sorteditems" :key="group.groupName" class="flex flex-col gap-3 mt-5">
+      <div class="flex items-center">
+        <span class="font-bold text-2xl text-gray-500 mr-5">{{ group.category }}</span>
+        <div class="flex-1 border"></div>
+      </div>
+    </div>
+
     <div v-for="data in filteredData" class="flex flex-col md:flex-row items-start shadow-lg bg-white rounded-lg">
-      
     <!-- image -->
     <div class="flex-1 flex flex-col gap-3 items-start justify-between">
     <div :style="{ backgroundImage: `url(${data.img})` }" class="bg-no-repeat  bg-center h-[250px] mt-2 ml-2 md:w-[300px] lg:w-[500px] relative after:h-[50px] after:bottom-0 after:w-full after:bg-gradient-to-t after:from-black/30 after:to-white/10 after:absolute after:blur-sm"> </div>
@@ -21,7 +27,7 @@
         {{ data.content }}
       </p>
       <!-- PreviewButton -->
-      <button @click="showTouristSpotPreviewModal(data)" class="bg-blue-600 hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs self-end mt-20 uppercase">view </button>
+      <button @click="showTouristSpotPreviewModal(data)" class="bg-blue-600 hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs self-end mt-20 uppercase">edit</button>
     </div>
   </div>
   
@@ -38,7 +44,7 @@
 </WrapperContent>
 </template>
 <script setup>
-import { ref, onUpdated } from 'vue';
+import { computed, ref, onUpdated } from 'vue';
 import SearchInput from '../../Components/SearchInput.vue';
 import AddModal from '../../Components/AddModal/AddModal.vue';
 import TouristSpotPreviewModal from '../../Components/TouristSpotPreviewModal.vue'
@@ -46,38 +52,63 @@ const isTouristSpotPreviewModal = ref(false);
 const isAddNewModal = ref(false);
 const selectedData = ref();
 
-
-
         const sample_data = [{
               placeName:"Pililla Wind Farm",
               content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
               img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/cd/67/c8/vast-view-of-the-windmills.jpg?w=1000&h=-1&s=1",
-              address: "Pililla Windmills - 54 MW Pililla Windfarm - Sitio Bugarin, Pililla Rizal"
+              address: "Pililla Windmills - 54 MW Pililla Windfarm - Sitio Bugarin, Pililla Rizal",
+              category: "Tourist Spot"
             },
-      {
+            {
               placeName:"Lyger Animal Sanctuary - ZOO",
               content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
               img: "https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-6/274579439_2419634788171057_6700194066240640258_n.jpg?stp=dst-jpg_p480x480&_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHUaaI-kyM3lhpYUkNDvkobeTkfTFeFN5F5OR9MV4U3kV80MePrW1SKq06pbp3sW46NZSuH74c8GFQv_JGBw2PG&_nc_ohc=vZzU1pyI1gIAX-769R_&_nc_ht=scontent.fmnl25-3.fna&oh=00_AfAimQATg8SzNiIGyE-4nRqoGtM7Iv-aEOfqIBbpgMCvcQ&oe=64157420",
-              address: "Matagbak Rd. 1910 Pililla, Philippines"
+              address: "Matagbak Rd. 1910 Pililla, Philippines",
+              category: "Tourist Spot"
             },
-      {
+            {
               placeName:"Mt. Sembrano",
               content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
               img: "http://tarakahitsaan.com/images/spots/mt-sembrano-rizal.jpg",
-              address: "99R6+574, Malaya Trail, Pililla, Riza"
+              address: "99R6+574, Malaya Trail, Pililla, Riza",
+              category: "Tourist Spot"
             },
-      {
+            {
               placeName:"St. Mary Magdalene Church",
               content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
               img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
-              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal"
+              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+              category: "Tourist Spot"
             },
             {
               placeName:"Bahay na Bato",
               content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
               img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-016.jpg",
-              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal"
-            }]
+              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+              category: "Tourist Spot"
+            },
+            {
+              placeName:"St. Mary Magdalene Church",
+              content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
+              img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
+              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+              category: "Restaurant"
+            },
+            {
+              placeName:"St. Mary Magdalene Church",
+              content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
+              img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
+              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+              category: "Restaurant"
+            },
+            {
+              placeName:"St. Mary Magdalene Church",
+              content:"Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
+              img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
+              address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+              category: "Restaurant"
+            },
+            ]
             // search Bar//
 const filteredData = ref(searchFilter(""));
 function searchFilter(value) {
@@ -120,5 +151,17 @@ onUpdated(() => {
 });
 
 
+const sorteditems = sample_data.reduce((groups, item) => {
+  const groupName = item.category;
+  if (!groups[groupName]) {
+    groups[groupName] = [];
+  }
+  groups[groupName].push(item);
+  return groups;
+}, {});
+
+Object.keys(sorteditems).sort().forEach((key) => {
+  sorteditems[key] = sorteditems[key].sort((a, b) => a.category.localeCompare(b.category));
+});
 
 </script>
