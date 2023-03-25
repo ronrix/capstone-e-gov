5fc2e98e7c16e70f869d24d4ff4c303c9ab97d35
Francisco Hinay <116621218+francishinay@users.noreply.github.com>
2023-03-25 22:52:49 +0800

    Added a modal in Services (Requirements)

---
+++ capstone-admin/resources/js/Pages/Services/Services Components/RequirementsModal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black bg-opacity-20 z-20">
+             <div class="bg-white sm:w-[700px] p-5 rounded-lg relative flex flex-col items-start">
+                 <!-- close btn -->
+                 <i @click="showRequirementModal"
+                     class="uil uil-times text-black hover:text-blue-500 text-xl absolute top-0 right-2 cursor-pointer"></i>
+                 <h4 class="font-bold text-xl text-gray-900">Add new requirement</h4>
+                 <p class="text-sm text-gray-500">Add new requirement for awareness of the citizen</p>
+                 <!-- form -->
+                 <form @submit="submitFn" class="w-full mt-3 flex flex-col gap-3">
+                     <label class="flex flex-col w-full">
+                         <span class="font-bold text-gray-500">Requiremnts</span>
+                         <p class="text-xs text-gray-500">add the new requirements</p>
+                         <input type="text" class="border w-full p-2 rounded-lg focus:outline-blue-600">
+                     </label>
+                     <label class="flex flex-col w-full">
+                         <input type="text" class="border w-full p-2 rounded-lg focus:outline-blue-600">
+                     </label>
+                     <label class="flex flex-col w-full">
+                         <input type="text" class="border w-full p-2 rounded-lg focus:outline-blue-600">
+                     </label>
+                     <label class="flex flex-col w-full">
+                         <input type="text" class="border w-full p-2 rounded-lg focus:outline-blue-600">
+                     </label>
+                     <input type="submit" value="Save"
+                         class="w-full bg-blue-600 hover:bg-blue-500 text-white font-bold p-2 rounded-lg uppercase cursor-pointer">
+                 </form>
+             </div>
+         </div>
+   </template>
+   
+   <script setup>
+   
+   function submitFn(e) {
+     console.log("you submit me");
+     e.preventDefault();
+   }
+   
+   defineProps({
+    showRequirementModal: Function,
+   })
+   </script>
