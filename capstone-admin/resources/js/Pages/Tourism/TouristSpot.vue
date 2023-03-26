263540cc6a6afad6c5495cef39f954537f45cc0a
ronrix <127171334+Sazurc@users.noreply.github.com>
2023-03-27 00:02:52 +0800

    TouristSpot

--- capstone-admin/resources/js/Pages/Tourism/TouristSpot.vue
+++ capstone-admin/resources/js/Pages/Tourism/TouristSpot.vue
@@ -16,10 +16,8 @@
     <div v-for="data in filteredData" class="relative bg-white p-5 flex flex-col md:flex-row border border-r-0 border-y-0 border-l-[5px] border-l-blue-600 overflow-hidden shadow-md">
       <Delete :handleDelete="handleDelete" :id="data.id"  />
       <!-- image -->
-      <div class="flex-1 flex flex-col gap-3 items-start justify-between ">
-        <div :style="{ backgroundImage: `url(${data.img})` }"
-          class="bg-no-repeat overflow-hidden bg-center h-[250px] mt-2 ml-2 md:w-[300px] lg:w-[500px] relative after:h-[50px] after:bottom-0 after:w-full after:bg-gradient-to-t after:from-black/30 after:to-white/10 after:absolute after:blur-sm">
-        </div>
+      <div class="img-hover-zoom flex-1 flex flex-col gap-3 items-start justify-between ">
+          <img class="h-[300px] max-h-[400px] w-full lg:w-[max]" :src="data?.img">
         <div class="flex">
           <i style="margin-top: 2px;" class="ml-2 uil uil-location-point text-cyan-400"></i>
           <p class="text-ellipsis overflow-hidden pt-1" style="font-size: 12px;">{{ data.address }}</p>
@@ -28,12 +26,12 @@
       <!-- content -->
       <div class="flex-1 p-5 flex flex-col items-start justify-between">
         <h3 class="font-bold uppercase text-xl tracking-wide">{{ data.placeName }}</h3>
-        <p class="mt-5 text-ellipsis text-sm h-[100px] overflow-hidden">
+        <p class="text-ellipsis text-sm h-[100px] overflow-hidden">
           {{ data.content }}
         </p>
         <!-- PreviewButton -->
         <button @click="showTouristSpotPreviewModal(data)"
-          class="bg-blue-600 hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs self-end mt-20 uppercase">edit</button>
+          class="bg-blue-600 hover:bg-blue-500 px-3 py-2 text-white font-bold text-xs self-end mt-20 uppercase md:rounded ">edit</button>
       </div>
     </div>
 
@@ -72,7 +70,7 @@ const sample_data = [{
 {
   placeName: "Lyger Animal Sanctuary - ZOO",
   content: "Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
-  img: "https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-6/274579439_2419634788171057_6700194066240640258_n.jpg?stp=dst-jpg_p480x480&_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHUaaI-kyM3lhpYUkNDvkobeTkfTFeFN5F5OR9MV4U3kV80MePrW1SKq06pbp3sW46NZSuH74c8GFQv_JGBw2PG&_nc_ohc=vZzU1pyI1gIAX-769R_&_nc_ht=scontent.fmnl25-3.fna&oh=00_AfAimQATg8SzNiIGyE-4nRqoGtM7Iv-aEOfqIBbpgMCvcQ&oe=64157420",
+  img: "https://scontent.fmnl9-4.fna.fbcdn.net/v/t39.30808-6/274579439_2419634788171057_6700194066240640258_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_ohc=NAJUjJ_CHgMAX8f-Pic&_nc_ht=scontent.fmnl9-4.fna&oh=00_AfCW92uReO1yw3-pumvUz3Wt0aPHd-e8JKnXvcS-UjsO1Q&oe=64254620",
   address: "Matagbak Rd. 1910 Pililla, Philippines",
   category: "Tourist Attraction"
 },
@@ -98,25 +96,39 @@ const sample_data = [{
   category: "Tourist Spot"
 },
 {
-  placeName: "St. Mary Magdalene Church",
+  placeName: "Bulawan Floating Restaurant",
   content: "Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
-  img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
+  img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/9060Bulawan_Floating_Restaurant_46.jpg/800px-9060Bulawan_Floating_Restaurant_46.jpg?20190506074310",
   address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
   category: "Restaurant"
 },
 {
-  placeName: "St. Mary Magdalene Church",
+  placeName: "BALANGA RESTAURANT",
   content: "Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
-  img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
+  img: "https://img.restaurantguru.com/r40e-BALANGA-RESTAURANT-PILILLA-RIZAL-exterior.jpg",
   address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
   category: "Restaurant"
 },
 {
-  placeName: "St. Mary Magdalene Church",
+  placeName: "Al Fresko Grill and Resto Bar",
   content: "Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
-  img: "https://boyplakwatsa.files.wordpress.com/2014/04/rizal-tour-march-15-2014-orig-011.jpg?w=650",
+  img: "https://scontent.fmnl9-3.fna.fbcdn.net/v/t39.30808-6/278602678_885094395670809_7410699348354667593_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Tj3NESKNnR0AX8colhq&_nc_ht=scontent.fmnl9-3.fna&oh=00_AfBlosFVr7dW9w4lorNC95NSX2wHNLyXAATCPXNEzpqOaQ&oe=6425D954",
   address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
   category: "Restaurant"
+},
+{
+  placeName: "Villa Lorenza Resort",
+  content: "Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
+  img: "https://scontent.fmnl9-1.fna.fbcdn.net/v/t39.30808-6/334953929_231838572541594_3452519380656648569_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFmvU50yMUx-Q9UbNgvY0kvnwFC5kvFV9-fAULmS8VX33JTAL2B0Af4SOVnc3WgRE6wGjamVu--LKLY87hZ-NR1&_nc_ohc=YGcCVs22oK0AX-TqprU&_nc_ht=scontent.fmnl9-1.fna&oh=00_AfCefBSG6EEepD3JCADalg5_T5PwarEzH80qlg9iPkAnTA&oe=6424F32E",
+  address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+  category: "Resorts"
+},
+{
+  placeName: "Stagira Farms & Resort",
+  content: "Renewable energy is becoming more popular in the Philippines. As an alternative source of power and electricity, wind energy produces no waste and it is beneficial both for the people and the environment. It was in 2008 when the Department of Energy granted permission to Alternergy in creating a wind farm in the mountain slopes of Pililla, Rizal. Its purpose is to serve the nearby towns and municipalities of Rizal and Laguna with clean source of energy.",
+  img: "https://scontent.fmnl9-1.fna.fbcdn.net/v/t39.30808-6/274794166_494175622105227_7071963731837566068_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeH9WWSMXnJ745HxZ2u5rny02OCruIoSoJvY4Ku4ihKgmyLMsX59dIdMqtoC-Dp2mWz0Ehz5oaeUF6724izrvosM&_nc_ohc=RxCzGtjR16gAX-s0y-Y&_nc_ht=scontent.fmnl9-1.fna&oh=00_AfAnyNiNzKZxgKBc61GqcuyaoEacjLVSOel42FdHOINXTQ&oe=642597C1",
+  address: "Diocesan Shrine and Parish of St. Mary Magdalene - Imatong, Pililla, Rizal",
+  category: "Resorts"
 },
 ]
 const filterTourism = ['All', 'Tourist Attraction', 'Church', 'Restaurant']
@@ -175,3 +187,17 @@ onUpdated(() => {
 // });
 
 </script>
+<style scoped>
+.img-hover-zoom {
+  overflow: hidden; 
+}
+
+.img-hover-zoom img {
+  transition: transform .5s ease;
+}
+.img-hover-zoom:hover img {
+  transform: scale(1.5);
+
+}
+
+</style>
