28e369a5bebcd1032dcf0673356042e21e7c6372
ronrix <medyochain@gmail.com>
2023-03-27 22:26:57 +0800

    init setup capstone public view

---
+++ capstone-public/resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-blue-600">
+    hello, world
+  </div>
+</template>
+
+<script setup>
+
+</script>
--- capstone-public/resources/js/app.js
+++ capstone-public/resources/js/app.js
@@ -1,2 +1,20 @@
-import './bootstrap';
-import './tailwind-config';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+
+import '../css/app.css';
+import { router } from './router';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        const app = createApp({ render: () => h(App, props) });
+        app.use(router);
+        app.use(plugin);
+        app.mount(el);
+        
+    },
+    title: title => `${title} | Pililla`,
+});
