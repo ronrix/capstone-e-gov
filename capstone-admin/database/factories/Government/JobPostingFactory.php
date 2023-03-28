a72f9942da5785d8524e1156320e44d055c10db0
ronrix <medyochain@gmail.com>
2023-03-28 19:29:51 +0800

    add get all job postings on controller method and fix the migration table by adding schedules and adding approriate data on job postings factory

--- capstone-admin/database/factories/Government/JobPostingFactory.php
+++ capstone-admin/database/factories/Government/JobPostingFactory.php
@@ -16,11 +16,20 @@ class JobPostingFactory extends Factory
      */
     public function definition()
     {
+        $job_types = [
+            "developer",
+            "designer",
+            "accounting",
+            "helper",
+            "welder",
+        ];
+
         return [
             "job_title" => fake()->jobTitle(),
             "job_place" => fake()->streetAddress(),
             "job_description" => fake()->paragraph(5),
-            "job_type" => "remote, temporary, contractual, fixed term, full time",
+            "job_type" => fake()->randomElement($job_types),
+            "job_schedule" => "remote, temporary, contractual, fixed term, full time, part time, internship",
             "job_qualifications" => fake()->paragraph(5),
             "job_salary" => fake()->randomDigit(),
         ];
