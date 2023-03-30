d15116bc60f44d96f953cca4b447a964d9406f51
ronrix <medyochain@gmail.com>
2023-03-30 13:00:12 +0800

    fix style. adding w-full to the input tag

--- capstone-admin/resources/js/Components/SearchInput.vue
+++ capstone-admin/resources/js/Components/SearchInput.vue
@@ -3,7 +3,7 @@
   <div class="w-full flex items-center">
     <label class="w-full flex items-center rounded-lg p-2 text-sm bg-white">
       <i class="uil uil-search-alt pr-3"></i>
-      <input type="search" @change="$emit('searchFn', $event.target.value)" :placeholder="placeholder" class="bg-transparent outline-none" />
+      <input type="search" @change="$emit('searchFn', $event.target.value)" :placeholder="placeholder" class="bg-transparent outline-none w-full" />
     </label>
   </div>
 </template>
