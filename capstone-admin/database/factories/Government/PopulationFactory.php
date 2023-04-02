8b8c1c41361c3be26d342c0c392b36490f76ca40
ronrix <medyochain@gmail.com>
2023-04-02 23:15:16 +0800

    refactoring the columns of population table.

--- capstone-admin/database/factories/Government/PopulationFactory.php
+++ capstone-admin/database/factories/Government/PopulationFactory.php
@@ -16,14 +16,83 @@ class PopulationFactory extends Factory
      */
     public function definition()
     {
+
+        $religions = [
+            [ 
+                ["religion" => "roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["religion" => "roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["religion" => "roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["religion" =>"roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["religion" => "roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["religion" => "roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["religion" => "roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+            [ 
+                ["roman catholic", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "aglipay", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "iglesia ni cristo", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "islam", "count" => fake()->numberBetween(10000, 11000)],
+                ["religion" => "others", "count" => fake()->numberBetween(10000, 11000)],
+            ],
+        ];
+
+        $barangays = [
+            ["barangay" => "wawa", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "bagumbayan", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "halayhayin", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "hulo", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "imatong", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "malaya", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "niogan", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "quisao", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+            ["barangay" => "takungan", "male" => fake()->numberBetween(20000, 50000), "female" => fake()->numberBetween(20000, 50000), "household" => fake()->numberBetween(20000, 50000)],
+        ];
+
         return [
-            "barangays" => fake()->streetName(),
+            "barangays" => json_encode($barangays),
             "census_year" => fake()->year(),
-            "female" => fake()->randomDigit(),
-            "male" => fake()->randomDigit(),
-            "total_population" => fake()->randomDigit(),
-            "household" => fake()->randomDigit(),
-            "religion" => fake()->name()
+            "total_population" => fake()->numberBetween(200000, 500000),
+            "religion" => json_encode($religions)
         ];
     }
 }
