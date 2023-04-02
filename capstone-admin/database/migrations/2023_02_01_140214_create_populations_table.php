3a1cd1e1eb6a9a1d8ceed1d06bf3de8909a078c8
ronrix <medyochain@gmail.com>
2023-04-02 23:47:12 +0800

    adding soft delete feature

--- capstone-admin/database/migrations/2023_02_01_140214_create_populations_table.php
+++ capstone-admin/database/migrations/2023_02_01_140214_create_populations_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->year("census_year");
             $table->string("total_population");
             $table->json("religion");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
