80b3515c05902f1d2ed07125cf6ba3b17eac6282
ronrix <ronrixl5@gmail.com>
2023-04-04 03:13:11 +0800

    more css configs

--- capstone-public/tailwind.config.js
+++ capstone-public/tailwind.config.js
@@ -12,8 +12,13 @@ module.exports = {
       },
       colors: {
         primary: "#B92B27",
+        bggray: "#F2F2F2",
+        primarylight: "#CD4945",
         darkgray: "#434343",
         secondary: "#6E6E73",
+        bgLightyBlue: "#DFEBF7",
+        bgReddy: "#F3E8F2",
+        bgGreeny: "#DAF1F0",
       }
     },
   },
