ee78e329fe7c06d9b3e9526964efac6cdd6a271e
ronrix <ronrixl5@gmail.com>
2023-04-07 22:20:50 +0800

    adding a laravel routes for pages
    adding a comment in router/index.js. add your routes when you're working on you pages

--- capstone-public/resources/js/router/index.js
+++ capstone-public/resources/js/router/index.js
@@ -7,6 +7,11 @@ export const router = createRouter({
       path: "/",
       name: "Home",
       component: () => import("../Pages/Home/Home.vue")
-    }
+    },
+    /*
+    * add your route here
+    * copy the example above
+    * just modify it based on the components and routes that you're working on
+    */ 
   ]
 })
--- capstone-public/routes/web.php
+++ capstone-public/routes/web.php
@@ -17,3 +17,36 @@ Route::get('/', function () {
     $tz = new DateTime("now", new DateTimeZone("Asia/Manila"));
     return inertia('Main', ["time" => $tz->format("l, d F Y h:i:sA ")]);
 });
+
+// tourism
+Route::get('/tourism', function () {
+    return inertia('Main');
+});
+Route::get('/tourist-attractions', function () {
+    return inertia('Main');
+});
+Route::get('/festivals', function () {
+    return inertia('Main');
+});
+
+// business
+Route::get('/business', function () {
+    return inertia('Main');
+});
+Route::get('/establishments', function () {
+    return inertia('Main');
+});
+Route::get('/apartments', function () {
+    return inertia('Main');
+});
+Route::get('/bplo', function () {
+    return inertia('Main');
+});
+
+// services
+Route::get('/services', function () {
+    return inertia('Main');
+});
+Route::get('/scholarship', function () {
+    return inertia('Main');
+});
