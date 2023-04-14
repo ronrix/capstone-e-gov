f79f0999fc3037b9996840fb504eec8fe53d2d4c
ronrix <127171334+Sazurc@users.noreply.github.com>
2023-04-14 14:52:31 +0800

    Festivals

--- src/views/Tourism/Festivals.vue
+++ src/views/Tourism/Festivals.vue
@@ -6,12 +6,22 @@ import HeaderSection from '../../components/Header/HeaderSection.vue'
   <HeaderSection></HeaderSection>
   <div class="pt-28 grid">
     <div class="flex justify-between items-center ">
-        <p class="pl-20 text-8xl pt-10">Pililla Rizal<span class="block">Festival</span> </p>
-      <div class="border-2 h-[500px] flex gap-5 pr-20">
-        <div class="h-[400px] w-[250px] bg-slate-300 self-end"></div>
-        <div class="h-[400px] w-[250px] bg-gray-500 bottom-20"></div>
-        <div class="h-[400px] w-[250px] bg-cyan-300 self-end"></div>
+        <p class="pl-20 text-8xl pt-10">Pililla Rizal<span class="block font-['display'] italic text-red-600">Festival</span> </p>
+      <div class=" h-[500px] flex gap-5 pr-20">
+        <div class="drop-shadow-lg h-[400px] w-[250px] bg-slate-300 self-end bg-auto bg-no-repeat bg-center ..." style="background-image: url(https://scontent.fmnl25-2.fna.fbcdn.net/v/t1.6435-9/37954445_915526281972378_1758665335344463872_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHl_E-2KxR9Mc_qGpN0dQBFRxc4mjhGlv1HFziaOEaW_R8W0gha9nabo7Lg3AEZPItH337iJ1c6eUyB593SfraQ&_nc_ohc=fmIe0VATe7AAX8mv5ES&_nc_ht=scontent.fmnl25-2.fna&oh=00_AfBh5V6baEtMGWUtXn498QokUkYD-E4-bLrCncIMuacKvA&oe=645F50FB) ">
+          
+        </div>
+        <div class="drop-shadow-lg h-[400px] w-[250px] bg-slate-300 bg-auto bg-no-repeat bg-center ..." style="background-image: url(https://www.localguidesconnect.com/t5/image/serverpage/image-id/901602i9309756AF08A07CA/image-size/large?v=v2&px=999) ">
+          
+        </div>
+        <div class="drop-shadow-lg h-[400px] w-[250px] bg-slate-300 self-end bg-auto bg-no-repeat bg-center ..." style="background-image: url(https://static8.depositphotos.com/1386392/883/v/600/depositphotos_8836931-stock-illustration-two-dancing-girls-with-colorful.jpg) ">
+          
+        </div>
       </div>
+    </div>
+    <div class="h-[600px] w-full bg-slate-600">
+hello
+
     </div>
   </div>
   <FooterSection></FooterSection>
