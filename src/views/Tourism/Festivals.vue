308b87e5bc20756eb92b0b6a4237dbc9126fb952
ronrix <127171334+Sazurc@users.noreply.github.com>
2023-04-14 23:44:38 +0800

    Festival

--- src/views/Tourism/Festivals.vue
+++ src/views/Tourism/Festivals.vue
@@ -4,25 +4,71 @@ import HeaderSection from '../../components/Header/HeaderSection.vue'
 </script>
 <template>
   <HeaderSection></HeaderSection>
-  <div class="pt-28 grid">
-    <div class="flex justify-between items-center ">
-        <p class="pl-20 text-8xl pt-10">Pililla Rizal<span class="block font-['display'] italic text-red-600">Festival</span> </p>
-      <div class=" h-[500px] flex gap-5 pr-20">
-        <div class="drop-shadow-lg h-[400px] w-[250px] bg-slate-300 self-end bg-auto bg-no-repeat bg-center ..." style="background-image: url(https://scontent.fmnl25-2.fna.fbcdn.net/v/t1.6435-9/37954445_915526281972378_1758665335344463872_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHl_E-2KxR9Mc_qGpN0dQBFRxc4mjhGlv1HFziaOEaW_R8W0gha9nabo7Lg3AEZPItH337iJ1c6eUyB593SfraQ&_nc_ohc=fmIe0VATe7AAX8mv5ES&_nc_ht=scontent.fmnl25-2.fna&oh=00_AfBh5V6baEtMGWUtXn498QokUkYD-E4-bLrCncIMuacKvA&oe=645F50FB) ">
-          
-        </div>
-        <div class="drop-shadow-lg h-[400px] w-[250px] bg-slate-300 bg-auto bg-no-repeat bg-center ..." style="background-image: url(https://www.localguidesconnect.com/t5/image/serverpage/image-id/901602i9309756AF08A07CA/image-size/large?v=v2&px=999) ">
-          
-        </div>
-        <div class="drop-shadow-lg h-[400px] w-[250px] bg-slate-300 self-end bg-auto bg-no-repeat bg-center ..." style="background-image: url(https://static8.depositphotos.com/1386392/883/v/600/depositphotos_8836931-stock-illustration-two-dancing-girls-with-colorful.jpg) ">
-          
-        </div>
+  <div class="grid">
+    <div 
+    class=" flex justify-evenly items-center bg-auto bg-no-repeat bg-center contrast-150 ..."
+          style="
+            background-image: url();
+          " >
+          <div>
+            <p class="pl-20 text-8xl pt-10 ">
+        Pililla Rizal<span class="block font-['display'] italic text-red-600">Festival</span>
+      </p>
+          </div>
+          <div class="">
+            <img class="w-full rounded-bl-full" src="https://scontent.fmnl25-2.fna.fbcdn.net/v/t1.6435-9/37954445_915526281972378_1758665335344463872_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHl_E-2KxR9Mc_qGpN0dQBFRxc4mjhGlv1HFziaOEaW_R8W0gha9nabo7Lg3AEZPItH337iJ1c6eUyB593SfraQ&_nc_ohc=MGLmCzITthUAX-VwSNm&_nc_ht=scontent.fmnl25-2.fna&oh=00_AfD677bq_XdM4LiyaM3q8P8yHllkRfy4p9CxmK8eAlqfBg&oe=6460DABB" alt="">
+          </div>
+    </div>
+    <div class="h-[500px] flex pr-20 pb-10 ">
+      <div class="flex gap-5">
+        <div
+          class="drop-shadow-lg h-[400px] w-[250px] bg-red-400 self-end bg-auto bg-no-repeat bg-left text-8xl"
+          style="
+            background-image: url();
+          "
+        >Fes</div>
+        <div
+          class="drop-shadow-lg h-[400px] w-[250px] bg-red-400  bg-auto bg-no-repeat bg-center text-8xl"
+          style="
+            background-image: url();
+          "
+        >ti</div>
+        <div
+          class="drop-shadow-lg h-[400px] w-[250px] bg-red-400 self-end bg-auto bg-no-repeat bg-right text-8xl"
+          style="
+            background-image: url();
+          "
+        >val</div>
+      </div>
+      <p class="p-10 pt-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.</p>
+      </div>
+    <div class="">
+    <div class="h-[600px] w-full pt-2">
+
+      <div class="flex justify-evenly bg-white drop-shadow-md">
+      <img
+        class="w-[50%] contrast-125"
+        src="https://www.localguidesconnect.com/t5/image/serverpage/image-id/901602i9309756AF08A07CA/image-size/large?v=v2&px=999"
+        alt=""
+      />
+      <h2 class="text-5xl font-semibold pl-10 pt-5">
+        Magsolangtan Festival<span class="block w-[600px] font-normal text-sm mt-3"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.</span
+        >
+      </h2>
       </div>
     </div>
-    <div class="h-[600px] w-full bg-slate-600">
-hello
-
-    </div>
+  </div>
   </div>
   <FooterSection></FooterSection>
 </template>
