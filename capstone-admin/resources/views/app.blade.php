0198f335caa9c16836ac77ab2b5b6d42748fd7c1
ronrix <ronrixl5@gmail.com>
2023-04-20 18:28:51 +0800

    add tab logo

--- capstone-admin/resources/views/app.blade.php
+++ capstone-admin/resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="This is Municipality of Pililla Government Website" >
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="/images/logo/pililla-logo.png">
     {{-- fontawesom cdn --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- iconscout cdn --}}
