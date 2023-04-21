aca377359c1278c8409faeb933059f978bc35543
francishinay24 <Hinayhinaylang_24>
2023-04-21 17:49:47 +0800

    Added Business landing page

---
+++ src/views/Business/Landing page/Apartment.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  apartment: { type: Object, required: true },
+  idx: { type: Number, required: true }
+})
+</script>
+
+<template>
+  <div class="mx-10 flex items-center mt-1 gap-10">
+    <img
+      class="flex-1 w-1/2 h-[350px] rounded-lg"
+      :src="apartment?.imgSrc"  :class="{ 'order-first': idx % 2 === 0, 'order-last': idx % 2 !== 0 }"
+    />
+    
+    <div
+      class="flex flex-col flex-1 gap-2" :class="{ 'order-last': idx % 2 === 0, 'order-first': idx % 2 !== 0 }"
+    >
+      <p class="text-2xl font-semibold text-dark dark:text-white">Location at:</p>
+      <div class="flex items-center">
+        <i class="uil uil-map-marker text-red-700 text-2xl"></i>
+        <span class="text-lg font-light text-dark dark:text-white">{{ apartment?.location }}</span>
+      </div>
+      <button class="w-20 bg-primary p-1 rounded-md text-white text-sm mt-5 hover:bg-primarylight">
+        View
+        <i class="uil uil-angle-right"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Business/Landing page/ThirdSectionBusiness.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Permit from './Permit.vue';
+const permits = [{
+ name: 'Business Registration',
+ department: 'Business Permit and Licensing Office'
+},
+{
+ name: 'Issuance of MOA certification',
+ department: 'Agriculture Department'
+},
+{
+ name: 'Mayors Clearance',
+ department: 'Office of the mayor'
+}
+]
+</script>
+
+<template>
+    <div class="w-full bg-gray-700 dark:bg-gray-500 h-28 mt-10 flex items-center px-5 gap-2 mb-5" >
+        <h1 class="text-2xl text-white font-bold capitalize">Businesses permit</h1>
+        <i class="uil uil-file-check-alt text-4xl text-white"></i>
+    </div>
+        <!-- business permits -->
+    <Permit v-for="(data, id) in permits" :id="id" :key="id" :data="data" />
+    <button class="bg-primary hover:bg-primarylight text-sm rounded-md text-white ml-16 mt-10 py-3 px-5 mb-10">
+       View all businesses permits
+       <i class="uil uil-angle-right ml-2"></i>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
