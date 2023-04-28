832ef2e20cbdf70ef69bde0458b5991ad750457e
francishinay24 <Hinayhinaylang_24>
2023-04-28 22:44:26 +0800

    Fixed typo

--- src/views/About/Mission&Vision.vue
+++ src/views/About/Mission&Vision.vue
@@ -11,7 +11,7 @@ import HeaderSection from '../../components/Header/HeaderSection.vue'
   <WrapperContainer>
     <div class="sm:flex justify-evenly pt-10 ">
         <div>
-            <h1 class="text-dark text-3xl sm:text-5xl font-semibold">OUR MISSION <span class="block ">& Vission</span></h1>
+            <h1 class="text-dark text-3xl sm:text-5xl font-semibold">OUR MISSION <span class="block ">& Vision</span></h1>
         </div>
       <div class="grid gap-10 sm:w-1/2">
         <h2 class="grid gap-2 font-semibold">
@@ -23,7 +23,7 @@ import HeaderSection from '../../components/Header/HeaderSection.vue'
           >
         </h2>
         <h2 class="grid gap-2 font-semibold ">
-          Vission<span class="block font-normal text-sm"
+          Vision<span class="block font-normal text-sm"
             >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book. It has
