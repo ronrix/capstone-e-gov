40fbcbe1870f4967b1f94a0cff9b9f08385fb8a7
francishinay24 <Hinayhinaylang_24>
2023-04-30 22:45:24 +0800

    Update style

--- src/views/About/Mission and vision/MissionAndVision.vue
+++ src/views/About/Mission and vision/MissionAndVision.vue
@@ -11,11 +11,11 @@ import FooterSection from '../../../components/FooterSection/FooterSection.vue'
           <div class="flex flex-col mr-10 w-[50%] gap-5 lg:gap-10">
             <div class="flex flex-col">
                 <h2 class="text-primary dark:text-primarylight text-xl md:text-2xl xl:text-3xl font-bold mb-2 lg:mb-3 font-display">Mission</h2>
-                <p class="text-base lg:text-lg text-dark dark:text-white text-justify font-normal">“To improve the economic condition of the people of Pililla through the introduction of better economic opportunities, enhancement of revenue generation and better accessibility of basic services and needs.”</p>
+                <p class="text-base lg:text-lg text-dark dark:text-white text-justify font-light">“To improve the economic condition of the people of Pililla through the introduction of better economic opportunities, enhancement of revenue generation and better accessibility of basic services and needs.”</p>
             </div>
             <div class="flex flex-col">
                 <h2 class="text-primary dark:text-primarylight text-xl md:text-2xl xl:text-3xl font-bold mb-2 lg:mb-3 font-display">Vision</h2>
-                <p class="text-base lg:text-lg text-dark dark:text-white text-justify font-normal">“We envision Pililla as the alternative energy capital of the Philippines, with a God-fearing, peaceful, disaster resilient community, conducive to sustainable development and efficient management of natural and land resources through active, participative, responsive and committed leadership.”</p>
+                <p class="text-base lg:text-lg text-dark dark:text-white text-justify font-light">“We envision Pililla as the alternative energy capital of the Philippines, with a God-fearing, peaceful, disaster resilient community, conducive to sustainable development and efficient management of natural and land resources through active, participative, responsive and committed leadership.”</p>
             </div>
           </div>
           <div class="absolute bg-primary opacity-5 w-0 md:w-[310px] lg:w-[320px] md:h-[310px] lg:h-[320px] -left-24 lg:-left-2 rounded-full -z-10"></div>
