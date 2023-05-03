6686bc58d58ab57ec518331e237b5b8397a4a486
francishinay24 <Hinayhinaylang_24>
2023-05-03 12:51:10 +0800

    Fix style in text

--- src/views/About/Mission and vision/MissionAndVision.vue
+++ src/views/About/Mission and vision/MissionAndVision.vue
@@ -7,15 +7,15 @@ import FooterSection from '../../../components/FooterSection/FooterSection.vue'
 <template>
     <HeaderSection />
         <div class="w-full mt-10 flex flex-col  items-center mb-20 ">
-            <h1 class="text-center text-gray-800 dark:text-gray-400 font-bold text-4xl md:text-5xl mb-5 md:mb-10">Our <span class="text-dark dark:text-white"> Mission </span><span class="block"><span class="text-dark dark:text-white">&</span> Vision</span></h1>
-          <div class="flex flex-col mx-16 md:mx-32 lg:mx-44 gap-5">
+            <h1 class="text-center text-primary dark:text-primarylight font-bold text-3xl md:text-4xl mb-5 md:mb-10">Our Mission & Vision</h1>
+          <div class="flex flex-col mx-16 md:mx-32 lg:mx-44 gap-10">
             <div class="flex flex-col">
-                <h2 class="text-primary dark:text-primarylight text-xl sm:text-2xl md:text-3xl font-bold mb-2 md:mb-3 font-display">Mission</h2>
-                <p class="text-base lg:text-lg text-gray-700 dark:text-white text-justify font-normal">“To improve the economic condition of the people of Pililla through the introduction of better economic opportunities, enhancement of revenue generation and better accessibility of basic services and needs.”</p>
+                <h2 class="text-center text-dark dark:text-white text-xl md:text-2xl font-bold mb-2 md:mb-3 font-display">Mission</h2>
+                <p class="text-base lg:text-lg text-dark dark:text-white text-center font-normal">“To improve the economic condition of the people of Pililla through the introduction of better economic opportunities, enhancement of revenue generation and better accessibility of basic services and needs.”</p>
             </div>
             <div class="flex flex-col">
-                <h2 class="text-primary dark:text-primarylight text-xl sm:text-2xl md:text-3xl font-bold mb-3 md:mb-5 font-display">Vision</h2>
-                <p class="text-base lg:text-lg text-gray-700 dark:text-white text-justify font-normal">“We envision Pililla as the alternative energy capital of the Philippines, with a God-fearing, peaceful, disaster resilient community, conducive to sustainable development and efficient management of natural and land resources through active, participative, responsive and committed leadership.”</p>
+                <h2 class="text-center text-dark dark:text-white text-xl md:text-2xl font-bold mb-3 md:mb-5 font-display">Vision</h2>
+                <p class="text-base lg:text-lg text-dark dark:text-white text-center font-normal">“We envision Pililla as the alternative energy capital of the Philippines, with a God-fearing, peaceful, disaster resilient community, conducive to sustainable development and efficient management of natural and land resources through active, participative, responsive and committed leadership.”</p>
             </div>
         </div>
         </div>
--- src/views/Government/OfficialSeal/CardOfficialSeal.vue
+++ src/views/Government/OfficialSeal/CardOfficialSeal.vue
@@ -1,24 +1,25 @@
 <script setup>
 defineProps({
-    data: {type: Object, required: true},
-    idx: {type: Object, required: true}
+    data: { type: Object, required: true },
+    idx: { type: Object, required: true }
 })
 </script>
 
 <template>
     <div class="flex flex-col mt-2 rounded-md bg-white border border-gray-50 dark:bg-dark px-3 lg:px-5 py-7">
-    <div class="flex flex-col md:flex-row items-center mx-2 lg:mx-10 gap-10">
-            <div class="flex flex-col gap-3" :class="{'order-last md:order-first': idx % 2 ===0, 'order-last': idx % 2 !== 0}">
+        <div class="flex flex-col md:flex-row items-center mx-2 lg:mx-10 gap-10">
+            <div class="flex flex-col gap-3"
+                :class="{ 'order-last md:order-first': idx % 2 === 0, 'order-last': idx % 2 !== 0 }">
                 <h2 class="text-xl md:text-2xl font-bold text-dark-700 dark:text-white capitalize">{{ data?.logo }}</h2>
-                <h1 class="text-2xl md:text-3xl font-bold text-primary dark:text-primarylight uppercase" :class="{'text-blue-600 dark:text-blue-400': idx % 2 !== 0}">{{ data?.name }}</h1>
+                <h1 class="text-2xl md:text-3xl font-bold text-primary dark:text-primarylight uppercase"
+                    :class="{ 'text-blue-600 dark:text-blue-400': idx % 2 !== 0 }">{{ data?.name }}</h1>
                 <p class="flex-1 text-sm lg:text-base text-gray-500 dark:text-white">{{ data?.desc }}</p>
             </div>
-            <img class="" :src="data?.imgSrc"  alt="" :class="{'order-first md:order-last w-[300px]': idx % 2 ===0, 'order-first': idx % 2 !== 0}">
+            <img class="" :src="data?.imgSrc" alt=""
+                :class="{ 'order-first md:order-last w-[300px]': idx % 2 === 0, 'order-first': idx % 2 !== 0 }">
         </div>
     </div>
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
