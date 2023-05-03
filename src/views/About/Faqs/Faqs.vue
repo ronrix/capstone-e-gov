4764b52fa5bb3d8278182d9d1b8530e1f5b2bf27
francishinay24 <Hinayhinaylang_24>
2023-05-03 15:13:06 +0800

    Added FAQs page

---
+++ src/views/About/Faqs/Faqs.vue
@@ -0,0 +1,41 @@
+<script setup>
+import HeaderSection from '../../../components/Header/HeaderSection.vue'
+import FooterSection from '../../../components/FooterSection/FooterSection.vue'
+import Questions from './Questions.vue';
+
+const questions = [
+  {
+    question: 'How many department in your municipal?',
+    ans: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam distinctio sunt, reiciendis fugit blanditiis repellendus similique sit facilis iste id molestias vero illum ullam voluptatum accusamus hic ratione eos? Consequatur.'
+  },
+  {
+    question: 'How many department in your municipal?',
+    ans: 'answer 2'
+  },
+  {
+    question: 'How many department in your municipal?',
+    ans: 'answer 3'
+  }
+]
+</script>
+
+<template>
+  <HeaderSection />
+  <WrapperContainer>
+    <div class="w-full flex flex-col gap-5 items-center mt-10">
+      <p class="text-sm md:text-normal text-gray-700 dark:text-gray-400 font-bold uppercase">
+        faq<span class="lowercase">s</span>
+      </p>
+      <h1 class="text-dark dark:text-white font-light text-3xl md:text-4xl">Frequenlty asked questions</h1>
+      <h4 class="text-normal md:text-lg text-secondary dark:text-gray-200">Have questions? Were here to hep.</h4>
+    </div>
+    <!-- questions -->
+    <div class="flex flex-col mt-20 mx-1 md:mx-5 lg:mx-40">
+      <Questions v-for="(data,idx) in questions" :key="idx" :data="data"/>
+    </div>
+  </WrapperContainer>
+  <FooterSection />
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/About/Faqs/Questions.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+    data: {type: Object, required: true},
+    showAnswer: {type: Function, required:true}
+})
+
+function showAnswer(e) {
+  document.querySelectorAll('.answers').forEach((el) => {
+    if (e.target.nextElementSibling === el) {
+      return
+    }
+    el.classList.add('hidden')
+  })
+  e.target.nextElementSibling.classList.toggle('hidden')
+}
+</script>
+
+<template>
+    <div class=" flex flex-col mt-5 cursor-pointer" @click="showAnswer">
+        <div class="flex items-center justify-between pointer-events-none">
+            <h2 class="text-normal text-dark font-medium dark:text-white">
+            {{ data?.question }}
+        </h2>
+        <i class="uil uil-angle-down text-2xl"></i>
+        </div>
+      </div>
+      <div class="hidden answers">
+        <p class="w-[85%] text-secondary dark:text-gray-300 font-normal text-sm mt-2">
+         {{ data?.ans }}
+        </p>
+      </div>
+      <hr class="bg-gray-200 mt-6" />
+  
+    
+     
+     
+</template>
+
+<style lang="scss" scoped>
+
+</style>
