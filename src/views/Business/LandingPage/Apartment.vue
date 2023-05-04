1ab11e11ef50fbd2330574ab389a72be364f473e
francishinay24 <Hinayhinaylang_24>
2023-05-04 11:20:34 +0800

    Business landing page responsiveness

--- src/views/Business/LandingPage/Apartment.vue
+++ src/views/Business/LandingPage/Apartment.vue
@@ -6,19 +6,25 @@ defineProps({
 </script>
 
 <template>
-  <div class="mx-10 flex items-center mt-1 gap-10">
+  <div
+    class="flex flex-col md:flex-row items-start md:items-center mx-0 lg:mx-10 mt-1 gap-5 md:gap-10 mb-12 md:mb-0"
+  >
     <img
-      class="flex-1 w-1/2 h-[350px] rounded-lg"
-      :src="apartment?.imgSrc"  :class="{ 'order-first': idx % 2 === 0, 'order-last': idx % 2 !== 0 }"
+      class="flex-1 w-full md:w-1/2 h-[300px] lg:h-[350px] rounded-lg"
+      :src="apartment?.imgSrc"
+      :class="{ 'order-first': idx % 2 === 0, 'order-first md:order-last': idx % 2 !== 0 }"
     />
-    
+
     <div
-      class="flex flex-col flex-1 gap-2" :class="{ 'order-last': idx % 2 === 0, 'order-first': idx % 2 !== 0 }"
+      class="flex flex-col flex-1 gap-2"
+      :class="{ 'order-last': idx % 2 === 0, 'order-first': idx % 2 !== 0 }"
     >
-      <p class="text-2xl font-semibold text-dark dark:text-white">Location at:</p>
+      <p class="text-xl md:text-2xl font-semibold text-dark dark:text-white">Location at:</p>
       <div class="flex items-center">
         <i class="uil uil-map-marker text-red-700 text-2xl"></i>
-        <span class="text-lg font-light text-dark dark:text-white">{{ apartment?.location }}</span>
+        <span class="text-base md:text-lg font-light text-dark dark:text-white">{{
+          apartment?.location
+        }}</span>
       </div>
       <button class="w-20 bg-primary p-1 rounded-md text-white text-sm mt-5 hover:bg-primarylight">
         View
--- src/views/Business/LandingPage/SecondSectionBusiness.vue
+++ src/views/Business/LandingPage/SecondSectionBusiness.vue
@@ -1,29 +1,35 @@
 <script setup>
-import Apartment from './Apartment.vue';
-const apartments = [{
+import Apartment from './Apartment.vue'
+const apartments = [
+  {
     location: 'M.A Roxas St. Bagumbayan Pililla, Rizal',
     imgSrc: 'images/apartment.jpg'
-},
-{
+  },
+  {
     location: 'M.A Roxas St. Bagumbayan Pililla, Rizal',
     imgSrc: 'images/apartment.jpg'
-},
+  }
 ]
 </script>
 
 <template>
   <div class="flex items-center gap-5 mt-24 mb-5">
     <div class="flex-1 border border-gray-600 dark:border-white"></div>
-    <h1 class="text-3xl text-dark dark:text-white font-bold">Looking for apartment?</h1>
+    <h1 class="text-2xl lg:text-3xl text-primary sm:text-dark dark:text-white font-bold">
+      Looking for apartment?
+    </h1>
   </div>
- <Apartment v-for="(apartment, idx) in apartments" :key="idx" :apartment="apartment" :idx="idx" />
+  <Apartment v-for="(apartment, idx) in apartments" :key="idx" :apartment="apartment" :idx="idx" />
 
- <div class="flex flex-col items-center mt-12">
-    <h2 class="text-primary dark:text-white text-xl font-bold">Find more available apartments</h2>
-    <button class="bg-primary rounded-md px-2 py-1 text-white text-sm mt-2 hover:bg-primarylight">Find here
+  <div class="flex flex-col items-center mt-12">
+    <h2 class="text-primary dark:text-white text-base md:text-xl font-bold">
+      Find more available apartments
+    </h2>
+    <button class="bg-primary rounded-md px-2 py-1 text-white text-sm mt-2 hover:bg-primarylight">
+      Find here
       <i class="uil uil-angle-right"></i>
     </button>
- </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
