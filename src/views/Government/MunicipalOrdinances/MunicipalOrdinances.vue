8a1d34ecf23183945805f6cfb2f30edccea83f12
ronrix <ronrixl5@gmail.com>
2023-05-08 11:11:22 +0800

    add ordinance data

--- src/views/Government/MunicipalOrdinances/MunicipalOrdinances.vue
+++ src/views/Government/MunicipalOrdinances/MunicipalOrdinances.vue
@@ -2,16 +2,49 @@
 import HeaderSection from '../../../components/Header/HeaderSection.vue'
 import FooterSection from '../../../components/FooterSection/FooterSection.vue'
 import PDFViewer from 'pdf-viewer-vue'
+import { ref, onMounted } from 'vue'
+import { fetchData } from '../../../utils/axios-instance'
+import { be_url } from '../../../assets/config/config'
+
+const ordinances = ref([])
+const activeOrdinance = ref()
+const axiosCall = () => {
+  fetchData('/ordinances')
+    .then((data) => {
+      ordinances.value = data.ordinances
+      activeOrdinance.value = data.ordinances[0]
+      console.log(activeOrdinance.value)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+function changeOrdinance(id) {
+  activeOrdinance.value = ordinances.value.find((o) => o.id === id)
+  console.log(activeOrdinance.value)
+}
+
+onMounted(() => {
+  axiosCall()
+})
 </script>
 <template>
+  <head>
+    <title>Ordinances | Pililla Rizal</title>
+  </head>
+
   <HeaderSection />
   <WrapperContainer>
     <div class="flex items-center">
       <h1 class="text-2xl md:text-3xl py-10 font-bold dark:text-bggray">Municipal Ordinances</h1>
       <div class="flex-1 border-t-2 border-black dark:border-bggray ml-3"></div>
     </div>
-    <div class="flex flex-col sm:flex-row gap-5">
-      <PDFViewer class="!h-[800px]" source="/sample.pdf" @download="handleDownload" />
+    <h5 v-if="!activeOrdinance && !ordinances.length" class="font-bold text-2xl">
+      No ordinances yet!
+    </h5>
+    <div v-else class="flex flex-col sm:flex-row gap-5">
+      <PDFViewer class="!h-[800px]" :source="be_url + '/' + activeOrdinance?.pdf_path" />
       <div class="flex flex-col w-full sm:w-[400px]">
         <h2
           class="text-xl md:text-2xl font-bold border-b-2 border-black dark:border-bggray pb-2 dark:text-bggray"
@@ -20,17 +53,15 @@ import PDFViewer from 'pdf-viewer-vue'
         </h2>
 
         <div class="py-3 border-b-[1px] border-dark dark:border-bggray">
-          <h3 class="text-base md:text-lg font-semibold mb-2 dark:text-bggray">
-            Ordinance of Tourism
-          </h3>
-          <RouterLink
+          <button
+            v-for="(ord, index) in ordinances"
+            :key="index"
             class="line-clamp-2 text-xs sm:text-sm dark:text-bggray hover:text-primary dark:hover:text-primary"
-            to="/municipal-ordinances/ordinance"
-            >M.O. 600 – AN ORDINANCE CREATING A TRUST FUND FOR ALL PHILHEALTH PAYMENTS FOR TSEKAP
-            (PCB), TB-DOTS, MCP AND NCP TO GOVERNMENT-OWNED HEALTH CARE PROVIDERS AND REFLECTING
-            THAT THE CONCERNED HEALTH CARE PROVIDERS SHALL MANAGE THE TRUST FUND IN ACCORDANCE WITH
-            THE GUIDELINES SET BY THE PHILIPPINE HEALTH INSURANCE CORPORATION.</RouterLink
+            type="button"
+            @click="changeOrdinance(ord.id)"
           >
+            {{ ord.title }}
+          </button>
         </div>
       </div>
     </div>
