c5f2e2e54957751c473e5f58aad1641b90b51dd6
ronrix <127171334+Sazurc@users.noreply.github.com>
2023-05-10 21:34:08 +0800

    Tourism

--- src/views/Tourism/TourismPage.vue
+++ src/views/Tourism/TourismPage.vue
@@ -44,16 +44,27 @@ onMounted(() => {
       </div>
     </div>
     <div class="flex justify-center relative">
-      <img class="w-[600px] absolute bg-center object-cover -z-10" src="../../../public/images/Tourismbg-Circle.png" />
+      <img
+        class="w-[600px] absolute bg-center object-cover -z-10"
+        src="../../../public/images/Tourismbg-Circle.png"
+      />
       <div class="flex pt-5">
-        <img class="sm:w-[210px] w-[160px] z-10 object-cover" src="https://www.pngaaa.com/api-download/1853846" alt="" />
-        <img class="sm:w-[200px] w-[100px] -translate-x-14 object-cover"
+        <img
+          class="sm:w-[210px] w-[160px] z-10 object-cover"
+          src="https://www.pngaaa.com/api-download/1853846"
+          alt=""
+        />
+        <img
+          class="sm:w-[200px] w-[100px] -translate-x-14 object-cover"
           src="https://i.postimg.cc/63XWfN0Z/kisspng-travel-package-tour-woman-japan-vacation-rameshwaram-devbhoomi-5b3ab8e2ee6588-22845129153057.png"
-          alt="" />
+          alt=""
+        />
       </div>
     </div>
   </WrapperContainer>
-  <div class="flex justify-between h-auto w-full text-white bg-primary items-center p-5 -translate-y-5 sm:-translate-y-0">
+  <div
+    class="flex justify-between h-auto w-full text-white bg-primary items-center p-5 -translate-y-5 sm:-translate-y-0"
+  >
     <div class="text-center flex-1 px-3">
       <p class="pt-2 text-xs lg:text-sm">
         Travel around Pililla to the beauty of nature easily and safely.
@@ -71,8 +82,11 @@ onMounted(() => {
 
   <!-- banner -->
   <div class="w-full sm:pt-10 pb-10">
-    <img class="w-full object-cover"
-      src="https://pilillarizal.gov.ph/wp-content/uploads/2022/10/pililla_painting-1024x326.png" alt="" />
+    <img
+      class="w-full object-cover"
+      src="https://pilillarizal.gov.ph/wp-content/uploads/2022/10/pililla_painting-1024x326.png"
+      alt=""
+    />
   </div>
 
   <WrapperContainer>
@@ -84,12 +98,19 @@ onMounted(() => {
         <p class="text-xs pb-1 text-secondary">We have more destination you can</p>
         <form
           class="relative flex items-center justify-center drop-shadow-lg rounded-3xl text-sm bg-gray-100 text-dark px-3 py-2"
-          @submit.prevent="handleSearch">
+          @submit.prevent="handleSearch"
+        >
           <i class="uil uil-map-marker text-base text-dark mr-2" />
-          <input v-model="searchInput" class="bg-transparent w-full outline-none" type="text"
-            placeholder="Search destinations" />
-          <button type="submit"
-            class="w-12 h-10 rounded-full bg-primarylight flex items-center justify-center hover:bg-primary">
+          <input
+            v-model="searchInput"
+            class="bg-transparent w-full outline-none"
+            type="text"
+            placeholder="Search destinations"
+          />
+          <button
+            type="submit"
+            class="w-12 h-10 rounded-full bg-primarylight flex items-center justify-center hover:bg-primary"
+          >
             <i class="uil uil-search text-white text-base" />
           </button>
         </form>
@@ -99,16 +120,26 @@ onMounted(() => {
   <WrapperContainer>
     <Loading v-if="loading" class="w-10 h-10 mx-auto" />
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-10 gap-5">
-      <RouterLink v-for="data in tourism" :key="data.id" :to="'/tourist-attractions/' +
-        data.title.replace(/\s+/g, '_').replace(/\n/g, ' ').toLowerCase()
-        " class="flex flex-col border hover:drop-shadow-lg bg-white dark:bg-dark">
-        <img class="object-cover h-[250px] w-full" :src="formatImgs(data.img_link.split(','))" alt="" />
+      <RouterLink
+        v-for="data in tourism"
+        :key="data.id"
+        :to="
+          '/tourist-attractions/' +
+          data.title.replace(/\s+/g, '_').replace(/\n/g, ' ').toLowerCase()
+        "
+        class="flex flex-col border hover:drop-shadow-lg bg-white dark:bg-dark"
+      >
+        <img
+          class="object-cover h-[250px] w-full"
+          :src="formatImgs(data.img_link.split(','))"
+          alt=""
+        />
         <div class="py-5 px-3">
           <p class="font-semibold text-dark dark:text-bggray capitalize">
             {{ data.title }}
           </p>
           <div class="flex text-dark dark:text-bggray">
-            <i class="uil uil-map-marker text-cyan-300 relative bottom-[2px]" />
+            <i class="uil uil-map-marker text-primary relative bottom-[2px]" />
             <p class="text-sm ml-1 capitalize">
               {{ data.location }}
             </p>
