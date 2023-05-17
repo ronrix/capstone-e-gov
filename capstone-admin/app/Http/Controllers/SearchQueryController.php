bb46f9c5dee12757122dfe6792c567c37be4f417
ronrix <ronrixl5@gmail.com>
2023-05-17 22:59:29 +0800

    add try catch to handle error in search query controller

--- capstone-admin/app/Http/Controllers/SearchQueryController.php
+++ capstone-admin/app/Http/Controllers/SearchQueryController.php
@@ -17,30 +17,35 @@ class SearchQueryController extends Controller
     {
         $searchQuery = $request->input('query');
         // perform search logic using $query here
-        $tourism = Tourism::select('id', 'title', 'description')
-            ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
-        $festivals = Festival::select('id', 'title', 'description')
-            ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
-        $business = Business::select('id', 'title', 'description')
-            ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
-        $news = News::select('id', 'title', 'description')
-            ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
-        $job = JobPosting::select('id', 'job_title AS title', 'job_description AS description')
-            ->whereRaw("MATCH (job_title, job_description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
-        $services = Service::select('id', 'title', 'description')
-            ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
-        $events = ProgramsEvents::select('id', 'title', 'description')
-            ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
-            ->get()->toArray();
+        try {
+            $tourism = Tourism::select('id', 'title', 'description')
+                ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
+            $festivals = Festival::select('id', 'title', 'description')
+                ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
+            $business = Business::select('id', 'title', 'description')
+                ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
+            $news = News::select('id', 'title', 'description')
+                ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
+            $job = JobPosting::select('id', 'job_title AS title', 'job_description AS description')
+                ->whereRaw("MATCH (job_title, job_description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
+            $services = Service::select('id', 'title', 'description')
+                ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
+            $events = ProgramsEvents::select('id', 'title', 'description')
+                ->whereRaw("MATCH (title, description) AGAINST (? IN BOOLEAN MODE)", [$searchQuery])
+                ->get()->toArray();
 
-        // Combine the results from the users and products tables
-        $results = array_merge(["tourist-attractions" => $tourism], ["festivals" => $festivals], ["business" => $business], ["news" => $news], ["job" => $job], ["services" => $services], ["programs-events" => $events]);
+            // Combine the results from the users and products tables
+            $results = array_merge(["tourist-attractions" => $tourism], ["festivals" => $festivals], ["business" => $business], ["news" => $news], ["job" => $job], ["services" => $services], ["programs-events" => $events]);
+        } catch (\Throwable $err) {
+            //throw $th;
+            return response()->json(["res" => ["msg" => $err->getMessage(), "status" => 400]], 400);
+        }
 
         return response()->json(["results" => $results]);
     }
