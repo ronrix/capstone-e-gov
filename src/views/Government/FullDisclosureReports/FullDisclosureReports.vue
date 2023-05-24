c1181d2d17934c58606ef79c39073b0f4562b1a5
ronrix <ronrixl5@gmail.com>
2023-05-24 19:31:12 +0800

    refactor the pdf file rendering in reports and ordinances page

--- src/views/Government/FullDisclosureReports/FullDisclosureReports.vue
+++ src/views/Government/FullDisclosureReports/FullDisclosureReports.vue
@@ -4,13 +4,27 @@ import FooterSection from '../../../components/FooterSection/FooterSection.vue'
 import HotlinesTable from '../../../components/Hotlines/HotlinesTable.vue'
 import { ref, onMounted, onUnmounted } from 'vue'
 import { useHotlineStore } from '../../../stores/hotlines-store'
+import { fetchData } from '../../../utils/axios-instance'
+import { be_url } from '../../../assets/config/config'
 
 const store = useHotlineStore()
 const hotlines = ref()
 
+const fullDisclosures = ref([])
+const axiosCall = () => {
+  fetchData('/full-disclosure-reports')
+    .then((data) => {
+      fullDisclosures.value = data.full_disclosure
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 onMounted(() => {
   // scroll on top when this component rendered
   window.scrollTo(0, 0)
+
+  axiosCall()
 
   // add tab title
   document.title = 'Full Disclosure Reports | Pililla Rizal'
@@ -33,10 +47,14 @@ onUnmounted(() => {
     <WrapperContainer>
       <h1 class="text-5xl pt-4 pb-3 border-b-2 dark:text-white">Annual Budget</h1>
       <div class="flex flex-col gap-2 pb-5 pt-4 w-fit dark:text-white">
-        <a class="hover:text-primary" href="" target="_blank">2010 Annual Budget</a>
-        <a class="hover:text-primary" href target="_blank">2015 Annual Budget</a>
-        <a class="hover:text-primary" href="" target="_blank">2018 Annual Budget</a>
-        <a class="hover:text-primary" href="" target="_blank">2020 Annual Budget</a>
+        <a
+          v-for="reports in fullDisclosures"
+          :key="reports.id"
+          class="hover:text-primary"
+          :href="be_url + '/' + reports.filepath"
+          target="_blank"
+          >2010 Annual Budget</a
+        >
       </div>
     </WrapperContainer>
   </div>
--- src/views/Government/MunicipalOrdinances/MunicipalOrdinances.vue
+++ src/views/Government/MunicipalOrdinances/MunicipalOrdinances.vue
@@ -1,26 +1,19 @@
 <script setup>
 import HeaderSection from '../../../components/Header/HeaderSection.vue'
 import FooterSection from '../../../components/FooterSection/FooterSection.vue'
-import PDFViewer from 'pdf-viewer-vue'
 import { ref, onMounted, onUnmounted } from 'vue'
 import { fetchData } from '../../../utils/axios-instance'
 import { be_url } from '../../../assets/config/config'
 
 const ordinances = ref([])
-const activeOrdinance = ref()
 const axiosCall = () => {
   fetchData('/ordinances')
     .then((data) => {
       ordinances.value = data.ordinances
-      activeOrdinance.value = data.ordinances[0]
     })
     .catch((err) => {
       console.log(err)
     })
-}
-
-function changeOrdinance(id) {
-  activeOrdinance.value = ordinances.value.find((o) => o.id === id)
 }
 
 onMounted(() => {
@@ -42,51 +35,24 @@ onUnmounted(() => {
   <WrapperContainer>
     <div class="flex items-center">
       <h1 class="text-2xl md:text-3xl py-10 font-bold dark:text-bggray">Municipal Ordinances</h1>
-      <div class="flex-1 border-t-2 border-black dark:border-bggray ml-3"></div>
     </div>
     <h5 v-if="!activeOrdinance && !ordinances.length" class="font-bold text-2xl">
       No ordinances yet!
     </h5>
-    <div v-else class="flex flex-col sm:flex-row gap-5">
-      <PDFViewer class="!h-[800px]" :source="be_url + '/' + activeOrdinance?.pdf_path" />
-      <div class="flex flex-col w-full sm:w-[400px]">
-        <h2
-          class="text-xl md:text-2xl font-bold border-b-2 border-black dark:border-bggray pb-2 dark:text-bggray"
-        >
-          Odinances
-        </h2>
-
-        <div class="py-3 border-b-[1px] border-dark dark:border-bggray">
-          <button
-            v-for="(ord, index) in ordinances"
-            :key="index"
-            class="line-clamp-2 text-xs sm:text-sm dark:text-bggray hover:text-primary dark:hover:text-primary"
-            type="button"
-            @click="changeOrdinance(ord.id)"
-          >
-            {{ ord.title }}
-          </button>
-        </div>
-      </div>
+    <div v-else class="flex flex-col gap-5">
+      <h3 class="text-dark dark:text-bggray text-xl font-semibold border border-x-0 border-t-0">
+        List of ordinances
+      </h3>
+      <a
+        v-for="ord in ordinances"
+        :key="ord.id"
+        :href="be_url + '/' + ord.pdf_path"
+        class="line-clamp-2 text-xs sm:text-sm dark:text-bggray hover:text-primary dark:hover:text-primary w-fit"
+        target="_blank"
+      >
+        {{ ord.title }}
+      </a>
     </div>
   </WrapperContainer>
   <FooterSection />
 </template>
-<style>
-.pdf-viewer {
-  --iron-icon-height: 20px;
-  --iron-icon-width: 20px;
-  --viewer-icon-ink-color: rgb(189, 189, 189);
-  --viewer-pdf-toolbar-background-color: rgb(50, 54, 57);
-  --viewer-text-input-selection-color: rgba(255, 255, 255, 0.3);
-  --viewer-pdf-toolbar-height: 56px;
-  background-color: #ccc;
-  height: 100%;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  min-width: 50% !important;
-  z-index: 1;
-}
-</style>
