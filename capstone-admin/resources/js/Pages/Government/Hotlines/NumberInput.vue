81ea6c1dce4a49f1e5968a3d1c7f8ebeee470057
ronrix <ronrixl5@gmail.com>
2023-06-05 21:59:26 +0800

    fix error

--- capstone-admin/resources/js/Pages/Government/Hotlines/NumberInput.vue
+++ capstone-admin/resources/js/Pages/Government/Hotlines/NumberInput.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="flex-1 w-full flex items-center gap-2">
-        <span class="text-xs font-bold text-gray-800">{{ type }}</span>
-        <input @change.capture="(e) => showEditVerification(e.target.value, id, type)" v-model="number"
-            oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-            maxlength="11"
-            class="focus:outline-blue-500 py-1 w-full overflow-scroll no-scrollbar m-0 text-xs h-[40px] max-h-[40ppx] border" />
-    </div>
+  <div class="flex-1 w-full flex items-center gap-2">
+    <span class="text-xs font-bold text-gray-800">{{ type }}</span>
+    <input
+      @change.capture="(e) => showEditVerification(e.target.value, id, type)"
+      :value="number"
+      oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+      maxlength="11"
+      class="focus:outline-blue-500 py-1 w-full overflow-scroll no-scrollbar m-0 text-xs h-[40px] max-h-[40ppx] border"
+    />
+  </div>
 </template>
 
 <script setup>
 defineProps({
-    id: Number,
-    showEditVerification: Function,
-    type: String,
-    number: String
+  id: Number,
+  showEditVerification: Function,
+  type: String,
+  number: String,
 });
 </script>
